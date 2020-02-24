35dc1bd0985637074542da4a233e3c6837167cf8
Zuheb <zuheb.javed@holome.co.uk>
2020-02-24 17:15:51 +0000

    -Updated loading image -Added safe area scaler

---
+++ Assets/HoloMeV3/Scripts/SafeAreaRescaler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SafeAreaRescaler : MonoBehaviour
+{
+    void Start()
+    {
+        Rect area = Screen.safeArea;
+        RectTransform rect = GetComponent<RectTransform>();
+        rect.anchorMin = new Vector2(area.xMin, area.yMin);
+        rect.anchorMin = new Vector2(area.xMax, area.yMax);
+    }
+}
