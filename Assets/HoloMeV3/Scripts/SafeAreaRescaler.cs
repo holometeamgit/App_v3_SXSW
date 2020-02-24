85319f377f6db6018cc52396d1722498526a1c16
Zuheb <zuheb.javed@holome.co.uk>
2020-02-24 17:34:25 +0000

    -Fixed Scaler

--- Assets/HoloMeV3/Scripts/SafeAreaRescaler.cs
+++ Assets/HoloMeV3/Scripts/SafeAreaRescaler.cs
@@ -6,7 +6,11 @@ public class SafeAreaRescaler : MonoBehaviour
     {
         Rect area = Screen.safeArea;
         RectTransform rect = GetComponent<RectTransform>();
-        rect.anchorMin = new Vector2(area.xMin, area.yMin);
-        rect.anchorMin = new Vector2(area.xMax, area.yMax);
+        //rect.anchorMin = new Vector2(area.xMin, area.yMin);
+        //rect.anchorMax = new Vector2(area.xMax, area.yMax);
+
+        rect.anchorMin = new Vector2(0.5f, 0.5f);
+        rect.anchorMax = new Vector2(0.5f, 0.5f);
+        rect.sizeDelta = new Vector2(area.width, area.height);
     }
 }
