a87d1d6a0152edb608c73e95c5c6e6c4661b3e86
Zuheb <zed_jay_1@hotmail.com>
2020-03-20 19:19:51 +0000

    -Added android back button support

---
+++ Assets/HoloMeV3/Scripts/InvokeOnBackButtonAndroid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InvokeOnBackButtonAndroid : MonoBehaviour
+{
+#if UNITY_ANDROID
+
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        if (!button)
+        {
+            Debug.LogError("No button was attached to the gameobject");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (button??isActiveAndEnabled)
+            {
+                button?.onClick.Invoke();
+            }
+        }
+    }
+#endif
+}
