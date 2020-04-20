ae57d2414e98f3818cc44347af227b562a9ebbd9
Zuheb <zed_jay_1@hotmail.com>
2020-04-20 12:15:44 +0100

    -More panel functionality added

---
+++ Assets/HoloMeV2/Scripts/StreamingScripts/PnlChannelKey.cs
@@ -0,0 +1,66 @@
+﻿using DG.Tweening;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PnlChannelKey : MonoBehaviour
+{
+    [SerializeField]
+    DotManager dotManager;
+
+    [SerializeField]
+    UnityEvent OnCodePassed;
+
+    [SerializeField]
+    TextMeshProUGUI txtIncorrectCode;
+    [SerializeField]
+    TextMeshProUGUI txtEnterCode;
+
+    private void Start()
+    {
+        dotManager.OnPassCodeEntered += TestCode;
+    }
+
+    private void OnEnable()
+    {
+        dotManager.ToggleBoxSprites(false);
+        dotManager.ClearText();
+        ToggleIncorrectCodeText(false);
+    }
+
+    void TestCode(string code)
+    {
+        if (code == "1337")
+        {
+            OnCodePassed?.Invoke();
+        }
+        else
+        {
+            StartCoroutine(WrongCodeRoutine());
+        }
+    }
+
+    public void IncorrectCodeShake()
+    {
+        txtIncorrectCode.GetComponent<RectTransform>().DOShakeAnchorPos(.25f, new Vector3(40, 0, 0), 80);
+    }
+
+    public void ToggleIncorrectCodeText(bool show)
+    {
+        txtIncorrectCode.gameObject.SetActive(show);
+        txtEnterCode.gameObject.SetActive(!show);
+    }
+
+    IEnumerator WrongCodeRoutine()
+    {
+        ToggleIncorrectCodeText(true);
+        IncorrectCodeShake();
+        dotManager.ToggleBoxSprites(true);
+        yield return new WaitForSeconds(2);
+        dotManager.ToggleBoxSprites(false);
+        dotManager.ClearText();
+        dotManager.ActivateTextField();
+        ToggleIncorrectCodeText(false);
+    }
+}
---
+++ Assets/HoloMeV2/Scripts/StreamingScripts/PnlChannelName.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using System.Collections;
+using DG.Tweening;
+
+public class PnlChannelName : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField inputChannelName;
+
+    [SerializeField]
+    TextMeshProUGUI txtInvalidNameWarning;
+
+    [SerializeField]
+    TextMeshProUGUI txtEnterChannelName;
+
+    [SerializeField]
+    UnityEvent OnChannelNamePassed;
+
+    public static string ChannelName { get; private set; }
+
+    public void OnReadyPressed()
+    {
+        //Any verification and validation should go here
+        if (string.IsNullOrWhiteSpace(inputChannelName.text))
+        {
+            StartCoroutine(InvalidNameRoutine());
+        }
+        else
+        {
+            ChannelName = inputChannelName.text;
+            OnChannelNamePassed?.Invoke();
+        }
+    }
+
+    private void OnEnable()
+    {
+        inputChannelName.text = "";
+        inputChannelName.interactable = true;
+        ToggleIncorrectCodeText(false);
+    }
+
+    public void IncorrectCodeShake()
+    {
+        txtInvalidNameWarning.GetComponent<RectTransform>().DOShakeAnchorPos(.25f, new Vector3(40, 0, 0), 80);
+    }
+
+    public void ToggleIncorrectCodeText(bool show)
+    {
+        txtInvalidNameWarning.gameObject.SetActive(show);
+        txtEnterChannelName.gameObject.SetActive(!show);
+    }
+
+    IEnumerator InvalidNameRoutine()
+    {
+        ToggleIncorrectCodeText(true);
+        IncorrectCodeShake();
+        inputChannelName.interactable = false;
+        yield return new WaitForSeconds(2);
+        inputChannelName.text = "";
+        inputChannelName.interactable = true;
+        ToggleIncorrectCodeText(false);
+    }
+
+}
