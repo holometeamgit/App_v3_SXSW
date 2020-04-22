9b6f0ac0f44f9e226be798d4214ad56f0d616ea0
Zuheb <zed_jay_1@hotmail.com>
2020-04-22 18:25:56 +0100

    -Stream overlay controls all linked up -Countdown system and message done

---
+++ Assets/HoloMeV3/Scripts/CustomControls/ToggleSlider.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Button))]
+public class ToggleSlider : MonoBehaviour
+{
+    [SerializeField]
+    Image imgOn;
+
+    [SerializeField]
+    Image imgOff;
+
+    [SerializeField]
+    UnityEvent OnToggledOff;
+
+    [SerializeField]
+    UnityEvent OnToggledOn;
+
+    bool toggleOn;
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (toggleOn)
+            {
+                SetToOff();
+            }
+            else
+            {
+                SetToOn();
+            }
+        });
+    }
+
+    private void OnEnable()
+    {
+        SetToOff();
+    }
+
+    void FlipOnOffImages(bool on)
+    {
+        imgOn.enabled = on;
+        imgOff.enabled = !on;
+    }
+
+    void SetToOff()
+    {
+        FlipOnOffImages(false);
+        OnToggledOff?.Invoke();
+        toggleOn = false;
+    }
+
+    void SetToOn()
+    {
+        FlipOnOffImages(true);
+        OnToggledOn?.Invoke();
+        toggleOn = true;
+    }
+
+}
+
--- Assets/HoloMeV3/Scripts/StreamingScripts/PnlStreamOverlay.cs
+++ Assets/HoloMeV3/Scripts/StreamingScripts/PnlStreamOverlay.cs
@@ -3,6 +3,8 @@ using DG.Tweening;
 using TMPro;
 using NatShare;
 using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PnlStreamOverlay : MonoBehaviour
 {
@@ -16,17 +18,30 @@ public class PnlStreamOverlay : MonoBehaviour
     PnlGenericError pnlGenericError;
 
     [SerializeField]
-    TextMeshProUGUI txtCountdown;
+    TextMeshProUGUI txtCentreMessage;
 
     [SerializeField]
     CanvasGroup canvasGroup;
 
     [SerializeField]
+    Toggle toggleAudio;
+
+    [SerializeField]
+    Toggle toggleVideo;
+
+    [SerializeField]
     UnityEvent OnClose;
+
+    int countDown;
+    string tweenAnimationID = nameof(tweenAnimationID);
+    Coroutine countdownRoutine;
 
     private void OnEnable()
     {
         FadePanel(true);
+        toggleAudio.isOn = false;
+        toggleVideo.isOn = false;
+        txtCentreMessage.text = string.Empty;
     }
 
     public void OpenAsStreamer()
@@ -70,5 +85,71 @@ public class PnlStreamOverlay : MonoBehaviour
 
             payload.AddText($"Check out my stream in the HoloMe App by typing in the channel {PnlChannelName.ChannelName} app {appLink}");
         }
+    }
+
+    public void ToggleAudio(bool mute)
+    {
+        TogglePauseStream();
+    }
+
+    public void ToggleVideo(bool hideVideo)
+    {
+        TogglePauseStream();
+    }
+
+    void TogglePauseStream()
+    {
+        if (toggleVideo.isOn && toggleAudio.isOn)
+        {
+            AnimatedCentreTextMessage("Stream Paused");
+        }
+        else
+        {
+            AnimatedFadeOutMessage();
+        }
+    }
+
+    public void StartCountdown()
+    {
+        countdownRoutine = StartCoroutine(CountDown());
+    }
+
+    public void StopStream()
+    {
+        if (countdownRoutine != null)
+            StopCoroutine(countdownRoutine);
+    }
+
+    void StartStream()
+    {
+
+    }
+
+    IEnumerator CountDown()
+    {
+        countDown = 3;
+
+        while (countDown >= 0)
+        {
+            AnimatedCentreTextMessage(countDown > 0 ? countDown.ToString() : "Now Live");
+            AnimatedFadeOutMessage(.5f);
+            countDown--;
+            yield return new WaitForSeconds(1);
+        }
+
+        StartStream();
+    }
+
+    private void AnimatedCentreTextMessage(string message)
+    {
+        DOTween.Kill(tweenAnimationID);
+        txtCentreMessage.text = message;
+        txtCentreMessage.color = new Color(txtCentreMessage.color.r, txtCentreMessage.color.g, txtCentreMessage.color.b, 1);
+        txtCentreMessage.rectTransform.DOPunchScale(Vector3.one, .25f, 3).SetId(tweenAnimationID);
+    }
+
+    private void AnimatedFadeOutMessage(float delay = 0)
+    {
+        txtCentreMessage.DOFade(0, .5f).SetDelay(delay).SetId(tweenAnimationID);
     }
 }
