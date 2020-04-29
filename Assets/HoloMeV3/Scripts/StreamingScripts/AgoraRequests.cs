c6047a1536d7a40e3725f260cb598d18921bbc88
Zuheb <zed_jay_1@hotmail.com>
2020-04-29 11:51:47 +0100

    -Json request sorted for channels

--- Assets/HoloMeV3/Scripts/StreamingScripts/AgoraRequests.cs
+++ Assets/HoloMeV3/Scripts/StreamingScripts/AgoraRequests.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class AgoraRequests : MonoBehaviour
 {
-    string baseURL = "https://api.agora.io/dev?_ga=2.248837760.647778257.1587586617-241556992.1585304514";
-    //string baseURL = "https://api.agora.io/dev?_ga=2.39590436.647778257.1587586617-241556992.1585304514";
-    //string baseURL = "https://api.agora.io/dev/v1?_ga=2.210040338.647778257.1587586617-241556992.1585304514";
-    //string baseURL = "https://api.agora.io/dev/";
+    string baseURL = "https://api.agora.io/dev/";
 
     //below are accessed from top right click on username then restful api
     string customerID = "cca351a8559f4da688319b23865d5e78";
@@ -16,45 +14,19 @@ public class AgoraRequests : MonoBehaviour
 
     private void Awake()
     {
-        //StartCoroutine(GetChannelList());
-        //StartCoroutine(GetRequest("https://api.agora.io/dev/v1?_ga=2.9181618.647778257.1587586617-241556992.1585304514/projects/"));
-        StartCoroutine(GetRequest("https://api.agora.io/dev/v1/channel/" + appId));
+        RequestChannels();
     }
-
-    //IEnumerator GetChannelList()
-    //{
-    //    string finalURL = "https://api.agora.io/dev/v1/projects/";
-    //    //string finalURL = baseURL + "/projects/";
-    //    //string finalURL = baseURL + "/channel/de596f86fdde42e8a7f7a39b15ad3c82";
-    //    //string finalURL = baseURL + "/v1/channel/de596f86fdde42e8a7f7a39b15ad3c82";
-    //    //string finalURL = baseURL + "/v1/channel/{de596f86fdde42e8a7f7a39b15ad3c82}";
-
-    //    UnityWebRequest channelRequest = UnityWebRequest.Get(finalURL);
-    //    string authorizationDetails = customerID + ":" + certificate;
-    //    string encodedAuthDetails = Base64Encode(authorizationDetails);
-    //    channelRequest.SetRequestHeader("Authorization", encodedAuthDetails);
-
-    //    yield return channelRequest.SendWebRequest();
-
-    //    if (channelRequest.isNetworkError || channelRequest.isHttpError)
-    //    {
-    //        Debug.LogError(channelRequest.error);
-    //        yield break;
-    //    }
-
-    //    Debug.Log("JSON RECEIVED " + channelRequest.downloadHandler.text);
-    //}
 
     public static string Base64Encode(string plainText)
     {
         var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
         return System.Convert.ToBase64String(plainTextBytes);
     }
-    IEnumerator GetRequest(string uri)
+
+    IEnumerator GetRequest(string uri, Action<string> OnSuccess)
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
-
             string authorizationDetails = customerID + ":" + certificate;
             string encodedAuthDetails = Base64Encode(authorizationDetails);
             webRequest.SetRequestHeader("Authorization", "Basic " + encodedAuthDetails);
@@ -72,8 +44,39 @@ public class AgoraRequests : MonoBehaviour
             else
             {
                 Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text + webRequest.responseCode);
+                OnSuccess?.Invoke(webRequest.downloadHandler.text);
             }
         }
     }
 
+    void RequestChannels()
+    {
+        StartCoroutine(GetRequest(baseURL + "v1/channel/" + appId, OnChannelsReturned));
+    }
+
+    void OnChannelsReturned(string jsonText)
+    {
+        var response = JsonParser.CreateFromJSON<ChannelResponse>(jsonText);
+    }
+
+    [System.Serializable]
+    public class ChannelResponse
+    {
+        public bool success;
+        public ChannelDataStore data;
+    }
+
+    [System.Serializable]
+    public class ChannelDataStore
+    {
+        public ChannelData[] channels;
+        public int total_size;
+    }
+
+    [System.Serializable]
+    public struct ChannelData
+    {
+        public string channel_name;
+        public int user_count;
+    }
 }
