c4020bfa8961275b5b9995ff4003fe06929af413
Ivklim <ivan.klimov.92@gmail.com>
2020-10-11 01:46:27 +0300

    update pull refresh + add bottom pull refresh

--- Assets/HoloMeApp/Scripts/UI/ScrollList/UIPullRefreshScrollController.cs
+++ Assets/HoloMeApp/Scripts/UI/ScrollList/UIPullRefreshScrollController.cs
@@ -10,22 +10,37 @@ public class UIPullRefreshScrollController : MonoBehaviour
 
     [SerializeField] private UIRefreshableScroll scrollRect;
     [SerializeField] private float distanceRequiredRefresh = 200;
-    [SerializeField] private float distanceReachedBottom = -200;
+    [SerializeField] private float distanceReachedBottom = 700;
+    [SerializeField] private GameObject topLoadingInfo;
+    [SerializeField] private GameObject bottomLoadingInfo;
+    [SerializeField] bool withStartRefresh;
     private float initialPosition;      
     private float progress;
     private Vector2 stopPosition;
     private bool isPulled;
     private bool isRefreshing;
 
+    private bool isBottomRefreshing;
+    RectTransform scrollRectTransform; 
+
     public void EndRefreshing() {
         isPulled = false;
         isRefreshing = false;
+        isBottomRefreshing = false;
+        bottomLoadingInfo.SetActive(false);
+        topLoadingInfo.SetActive(true);
+        Debug.Log("EndRefreshing");
     }
 
     private void Start() {
         initialPosition = GetContentAnchoredPosition();
         stopPosition = new Vector2(scrollRect.content.anchoredPosition.x, initialPosition - distanceRequiredRefresh);
         scrollRect.onValueChanged.AddListener(OnScroll);
+        scrollRect.onValueChanged.AddListener(OnBottomPull);
+        scrollRectTransform = scrollRect.gameObject.GetComponent<RectTransform>();
+
+        if (withStartRefresh)
+            scrollRect.content.anchoredPosition += Vector2.down * distanceRequiredRefresh;
     }
 
     private void LateUpdate() {
@@ -51,7 +66,7 @@ public class UIPullRefreshScrollController : MonoBehaviour
             isRefreshing = false;
         }
 
-        if (isPulled && scrollRect.Dragging) {
+        if (isPulled && scrollRect.Dragging || isBottomRefreshing) {
             return;
         }
 
@@ -67,13 +82,33 @@ public class UIPullRefreshScrollController : MonoBehaviour
 
         if (isPulled && !scrollRect.Dragging) {
             isRefreshing = true;
+            Debug.Log("OnPull");
             OnRefresh?.Invoke();
         }
 
         progress = 0f;
     }
 
+    private void OnBottomPull(Vector2 normalizedPosition) {
+        if (isPulled || isRefreshing || isBottomRefreshing) {
+            return;
+        }
+
+        if (GetBottomAnchoredPosition() > distanceReachedBottom)
+            return;
+
+        isBottomRefreshing = true;
+        OnReachedBottom?.Invoke();
+        bottomLoadingInfo.SetActive(true);
+        topLoadingInfo.SetActive(false);
+        Debug.Log("OnBottomPull");
+    }
+
     private float GetContentAnchoredPosition() {
         return scrollRect.content.anchoredPosition.y;
+    }
+
+    private float GetBottomAnchoredPosition() {
+        return scrollRect.content.sizeDelta.y - scrollRect.content.anchoredPosition.y - scrollRectTransform.rect.height;
     }
 }
