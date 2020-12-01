ae42df65287982124a4068032c40ef0a8383e654
Ivklim <ivan.klimov.92@gmail.com>
2020-12-01 19:54:40 +0300

    open the buy panel when the teaser is closed

--- Assets/HoloMeApp/Scripts/UIThumbnailV3.cs
+++ Assets/HoloMeApp/Scripts/UIThumbnailV3.cs
@@ -74,6 +74,10 @@ public class UIThumbnailV3 : UIThumbnail {
 
     public override void PlayTeaser() {
         OnTeaserClick?.Invoke(thumbnailElement.Data);
+
+        //TODO just for V3 buy when start play teaser
+        if(!thumbnailElement.Data.is_bought)
+            Buy();
     }
 
     public override void Buy() {
