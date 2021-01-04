49273eb622886df4a7323e2968b0a40a4ed90de2
Zuheb <zed_jay_1@hotmail.com>
2021-01-04 11:26:44 +0000

    -Added environment preprocessors to analytics

--- Assets/HoloMeApp/Scripts/Analytics/AnalyticsController.cs
+++ Assets/HoloMeApp/Scripts/Analytics/AnalyticsController.cs
@@ -23,6 +23,7 @@ public class AnalyticsController : MonoBehaviour {
     }
 
     public void SendCustomEvent(string eventName) {
+#if LIVE
         if (string.IsNullOrWhiteSpace(eventName)) {
             Debug.LogError("Custom event name wasn't specified");
             return;
@@ -31,10 +32,12 @@ public class AnalyticsController : MonoBehaviour {
         HelperFunctions.DevLog($"Custom Event Sent {eventName}");
         Analytics.CustomEvent(eventName);
         CleverTapUnity.SendCustomEvent(eventName);
+#endif
     }
 
     public void SendCustomEvent(string eventName, string dataName, object data)
     {
+#if LIVE
         if (string.IsNullOrWhiteSpace(eventName))
         {
             Debug.LogError("Custom event name wasn't specified");
@@ -46,9 +49,11 @@ public class AnalyticsController : MonoBehaviour {
         HelperFunctions.DevLog($"Custom Event Sent {eventName} with data {dataName} {data}");
         Analytics.CustomEvent(eventName, dataContainer);
         CleverTapUnity.SendCustomEvent(eventName, dataContainer);
+#endif
     }
 
     public void StartTimer(string timerKey, string timerName) {
+#if LIVE
         if (dwellTimers.ContainsKey(timerKey)) {
             Debug.LogError("Timer already exists in collection " + timerKey);
             return; //dwellTimers[timerName];
@@ -57,6 +62,7 @@ public class AnalyticsController : MonoBehaviour {
         dwellTimer.trackerName = timerName;
         dwellTimer.StartTimer();
         dwellTimers.Add(timerKey, dwellTimer);
+#endif
 
         HelperFunctions.DevLog($"Added timer {timerKey}");
 
@@ -64,6 +70,7 @@ public class AnalyticsController : MonoBehaviour {
     }
 
     public void StopTimer(string timerName) {
+#if LIVE
         if (!dwellTimers.ContainsKey(timerName)) {
             Debug.LogError("Timer didn't exist in collection " + timerName);
             return;
@@ -71,9 +78,11 @@ public class AnalyticsController : MonoBehaviour {
 
         var timer = dwellTimers[timerName];
         RemoveTimer(timer, timerName, timer.trackerName, timer.Timer);
+#endif
     }
 
     public void StopTimer(string timerName, float customTime) {
+#if LIVE
         if (!dwellTimers.ContainsKey(timerName)) {
             Debug.LogError("Timer didn't exist in collection " + timerName);
             return;
@@ -81,13 +90,14 @@ public class AnalyticsController : MonoBehaviour {
 
         var timer = dwellTimers[timerName];
         RemoveTimer(timer, timerName, timer.trackerName, customTime);
+#endif
     }
 
     /// <param name="timerDictonaryKey">Required to remove timer</param>
     /// <param name="timerName">Name to be shown in analytics</param>
     /// <param name="elapsedTime">Time specified</param>
     private void RemoveTimer(AnalyticsDwellTracker dwellTimercomponent, string timerDictonaryKey, string timerName, float elapsedTime) {
-
+#if LIVE
         var time = new Dictionary<string, object>();
         time.Add(timerName, elapsedTime);
         Analytics.CustomEvent(timerName, time);
@@ -95,31 +105,38 @@ public class AnalyticsController : MonoBehaviour {
 
         HelperFunctions.DevLog($"Dwell timer {timerName} stopped. Time tracked = {elapsedTime}");
         Destroy(dwellTimercomponent);
+#endif
     }
 
     public int GetElapsedTime(string timerName) {
         if (!dwellTimers.ContainsKey(timerName)) {
+#if LIVE
             Debug.LogError("Timer didn't exist in collection " + timerName);
+#endif
             return 0;
         }
         return dwellTimers[timerName].Timer;
     }
 
     public void PauseTimer(string timerName) {
+#if LIVE
         if (!dwellTimers.ContainsKey(timerName)) {
             Debug.LogError("Timer didn't exist in collection " + timerName);
             return;
         }
 
         dwellTimers[timerName].PauseTimer();
+#endif
     }
 
     public void ResumeTimer(string timerName) {
+#if LIVE
         if (!dwellTimers.ContainsKey(timerName)) {
             Debug.LogError("Timer didn't exist in collection " + timerName);
             return;
         }
 
         dwellTimers[timerName].ResumeTimer();
+#endif
     }
 }
