1d99e4b0e18bd9693b69c97150d5a10439709ce2
IvanKlimov1 <ivan.klimov@holo.me>
2021-01-06 21:37:54 +0300

    Add simple internet connection check

---
+++ Assets/HoloMeApp/Scripts/RequestHandler/InternetConnectionChecker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InternetConnectionChecker : MonoBehaviour {
+    private const float DEFAULT_COOLDOWN = 5.0f;
+    private const float FAST_COOLDOWN = 1.0f;
+
+    private float currentCooldown;
+    [SerializeField]
+    private GameObject internetConnactionLostGO;
+
+    private void Start() {
+        currentCooldown = DEFAULT_COOLDOWN;
+        StartCoroutine(CheckingInternet());
+    }
+
+    private void CheckInternetConnection() {
+        bool internetIsLost = Application.internetReachability == NetworkReachability.NotReachable;
+        internetConnactionLostGO.SetActive(internetIsLost);
+        currentCooldown = internetIsLost ? FAST_COOLDOWN : DEFAULT_COOLDOWN;
+    }
+
+    IEnumerator CheckingInternet() {
+        while (true) {
+            CheckInternetConnection();
+            yield return new WaitForSeconds(currentCooldown);
+        }
+    }
+
+    private void OnApplicationPause(bool pause) {
+        if(pause) {
+            StopAllCoroutines();
+        } else {
+            StartCoroutine(CheckingInternet());
+        }
+    }
+}
