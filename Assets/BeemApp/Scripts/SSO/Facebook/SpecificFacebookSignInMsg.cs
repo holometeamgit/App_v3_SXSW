cf82c192a3670118cd3a968be9d3b244881396cf
IvanKlimov1 <ivan.klimov@holo.me>
2021-02-02 17:38:24 +0300

    update msg for user about facebook problem

--- Assets/BeemApp/Scripts/SSO/Facebook/SpecificFacebookSignInMsg.cs
+++ Assets/BeemApp/Scripts/SSO/Facebook/SpecificFacebookSignInMsg.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class SpecificFacebookSignInMsg : MonoBehaviour
 {
     public static string Title = "Recommendation";
-    public static string SpecificMsg = "Perhaps you previously registered in a different way using the same email that is linked to your Facebook account. Please use the previous login method.";
+    public static string SpecificMsg = "Your Facebook password has been reset. Please log in with the email address associated to you Facebook account and enter the password manually.";
 }
