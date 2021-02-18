0643a590c7fe7e954e949bb26c66912feba6ca6f
Zuheb <zed_jay_1@hotmail.com>
2021-02-18 12:37:58 +0000

    -Fixed string status

--- Assets/HoloMeApp/Scripts/StreamingScripts/SecondaryServerCalls.cs
+++ Assets/HoloMeApp/Scripts/StreamingScripts/SecondaryServerCalls.cs
@@ -122,7 +122,7 @@ public class SecondaryServerCalls : MonoBehaviour {
         RoomJsonPutData data = new RoomJsonPutData();
         data.agora_sid = requestCloudRecordResource.CloudRecordResponseData.sid;
         data.agora_channel = requestCloudRecordResource.StartCloudRecordRequestData.cname;
-        data.status = "Live";
+        data.status = "live";
         webRequestHandler.PutRequest(webRequestHandler.ServerURLMediaAPI + videoUploader.PutRoom, data, WebRequestHandler.BodyType.JSON, (x, y) => webRequestHandler.LogCallback(x, "Put Room callback: "+ y), (x,y) => webRequestHandler.ErrorLogCallback(x,"Put Room error callback: " + y), accountManager.GetAccessToken().access);
     }
 
