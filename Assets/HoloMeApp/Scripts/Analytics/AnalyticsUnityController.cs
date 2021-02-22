9eea427c25e8aa7f467949a42c839410aeb5c91d
Zuheb <zed_jay_1@hotmail.com>
2021-02-22 16:39:56 +0000

    -Added Unity analytics controller

---
+++ Assets/HoloMeApp/Scripts/Analytics/AnalyticsUnityController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Analytics;
+
+public class AnalyticsUnityController : AnalyticsLibraryAbstraction
+{
+    public override void SendCustomEvent(string eventName)
+    {
+        Analytics.CustomEvent(eventName);
+    }
+
+    public override void SendCustomEvent(string eventName, Dictionary<string, string> data)
+    {
+        Dictionary<string, object> convertedData = new Dictionary<string, object>();
+        foreach (KeyValuePair<string, string> eventData in data)
+        {
+            convertedData.Add(eventData.Key, eventData.Value);
+        }
+        Analytics.CustomEvent(eventName, convertedData);
+    }
+}
