68c55628e385208149fc5c47f984549d14a42c62
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-02-26 16:45:24 +0700

    send link

--- Assets/HoloMeApp/Scripts/ShareManager.cs
+++ Assets/HoloMeApp/Scripts/ShareManager.cs
@@ -19,9 +19,6 @@ public class ShareManager : MonoBehaviour {
         AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyShareEventPressed);
 
         using (var payload = new SharePayload()) {
-            string appName = "Beem";
-
-            string message = $"Click the link below to download the {appName} app which lets you experience human holograms using augmented reality: ";
             payload.AddText(aditionalInformation);
         }
     }
@@ -30,7 +27,7 @@ public class ShareManager : MonoBehaviour {
         string msg = "Come to my room: " + link;
         HelperFunctions.DevLog(msg);
 #if !UNITY_EDITOR
-        ShareStream(msg);
+        ShareStream(link);
 #endif
     }
 }
