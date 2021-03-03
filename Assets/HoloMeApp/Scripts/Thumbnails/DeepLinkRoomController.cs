d534c6bc7ee31650e0ddb8a5ba728511efcb2a20
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-03-03 13:01:22 +0700

    fix dynamic links

--- Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkRoomController.cs
+++ Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkRoomController.cs
@@ -25,42 +25,8 @@ public class DeepLinkRoomController : MonoBehaviour {
             RoomJsonData roomJsonData = JsonUtility.FromJson<RoomJsonData>(body);
             //room?roomid=string
             string uri = serverURLAPIScriptableObject.FirebaseDynamicLinkAPI + "/" + roomJsonData.id;
-            CreateLink(serverURLAPIScriptableObject.FirebaseDynamicLinkAPI, uri, StreamCallBacks.onMyRoomLinkReceived);
+            DynamicLinksCallBacks.onCreateShortLink?.Invoke(serverURLAPIScriptableObject.FirebaseDynamicLinkAPI, uri);
         } catch { }
-    }
-
-    private void CreateLink(string prefix, string baseLink, Action<string> onMyRoomLinkReceived) {
-        HelperFunctions.DevLog("baseLink = " + baseLink);
-        HelperFunctions.DevLog("prefix = " + prefix);
-        var components = new DynamicLinkComponents(
-      // The base Link.
-      new Uri(baseLink),
-      // The dynamic link URI prefix.
-      prefix) {
-            IOSParameters = new IOSParameters(Application.identifier),
-            AndroidParameters = new AndroidParameters(Application.identifier),
-        };
-
-        var options = new DynamicLinkOptions {
-            PathLength = DynamicLinkPathLength.Short
-        };
-
-        var taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
-        DynamicLinks.GetShortLinkAsync(components, options).ContinueWith(task => {
-            if (task.IsCanceled) {
-                Debug.LogError("GetShortLinkAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted) {
-                Debug.LogError("GetShortLinkAsync encountered an error: " + task.Exception);
-                return;
-            }
-
-            // Short Link has been created.
-            ShortDynamicLink link = task.Result;
-            Debug.LogFormat("Generated short link: {0}", link.Url);
-            onMyRoomLinkReceived?.Invoke(link.Url.AbsoluteUri);
-        }, taskScheduler);
     }
 
     private void Awake() {
