8eff535718d77e6fa22f6efca603ee40b6b022cc
IvanKlimov1 <ivan.klimov@holo.me>
2021-03-12 20:45:13 +0300

    fix deep link

--- Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkRoomChecker.cs
+++ Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkRoomChecker.cs
@@ -7,17 +7,31 @@ public class DeepLinkRoomChecker : MonoBehaviour
     [SerializeField] Switcher SwitcherToRoomBroadcastHoldingScreen;
     [SerializeField] RoomLinkHandler roomLinkHandler;
 
+    private float DELAY_TO_SHOW_BROADCASTER_HOLDING_SCREEN = 0.5f;
+
     private void ShowBroadcastHoldingScreen() {
-        if (gameObject.activeInHierarchy && roomLinkHandler.HasRoomId())
+        HelperFunctions.DevLog("check ShowBroadcastHoldingScreen");
+        if (gameObject.activeInHierarchy && roomLinkHandler.HasRoomId()) {
             SwitcherToRoomBroadcastHoldingScreen.Switch();
+
+            HelperFunctions.DevLog("ShowBroadcastHoldingScreen");
+        }
     }
 
     private void OnEnable() {
-        ShowBroadcastHoldingScreen();
+        StartCoroutine(ShowBroadcastHoldingScreenWithDelay());
         StreamCallBacks.onOpenRoom += ShowBroadcastHoldingScreen;
     }
 
     private void OnDisable() {
+        StopAllCoroutines();
         StreamCallBacks.onOpenRoom -= ShowBroadcastHoldingScreen;
+    }
+
+    private IEnumerator ShowBroadcastHoldingScreenWithDelay() {
+        yield return new WaitForSeconds(DELAY_TO_SHOW_BROADCASTER_HOLDING_SCREEN);
+
+        if (gameObject.activeInHierarchy)
+            ShowBroadcastHoldingScreen();
     }
 }
