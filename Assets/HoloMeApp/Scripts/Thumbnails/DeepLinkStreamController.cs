e16e15aed394acb2585583b04ef0170e153cdc82
IvanKlimov1 <ivan.klimov@holo.me>
2021-04-10 00:00:13 +0300

    add share stream script

---
+++ Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkStreamController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Beem.Firebase.DynamicLink;
+
+public class DeepLinkStreamController : MonoBehaviour
+{
+    [SerializeField] ServerURLAPIScriptableObject serverURLAPIScriptableObject;
+
+    private void Awake() {
+        StreamCallBacks.onGetStreamLink += GetStreamLink;
+    }
+
+    private void GetStreamLink(string id) {
+        DynamicLinksCallBacks.onCreateShortLink?.Invoke(serverURLAPIScriptableObject.FirebaseDynamicLink, serverURLAPIScriptableObject.Stream, id, serverURLAPIScriptableObject.Url);
+    }
+
+    private void OnDestroy() {
+        StreamCallBacks.onGetStreamLink -= GetStreamLink;
+    }
+}
