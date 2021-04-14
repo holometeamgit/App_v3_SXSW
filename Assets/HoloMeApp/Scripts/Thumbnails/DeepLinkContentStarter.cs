b1b74c87e799f18c63603efedc6f3384f9aa12f0
IvanKlimov1 <ivan.klimov@holo.me>
2021-04-14 14:49:21 +0300

    fox event open stream

--- Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkContentStarter.cs
+++ Assets/HoloMeApp/Scripts/Thumbnails/DeepLinkContentStarter.cs
@@ -9,8 +9,6 @@ public class DeepLinkContentStarter : MonoBehaviour
     [SerializeField]
     ContentLinkHandler contentLinkHandler;
     [SerializeField]
-    WebRequestHandler webRequestHandler;
-    [SerializeField]
     VideoUploader videoUploader;
     [SerializeField]
     AccountManager accountManager;
@@ -21,58 +19,50 @@ public class DeepLinkContentStarter : MonoBehaviour
 
     private float DELAY_TO_SHOW_BROADCASTER_HOLDING_SCREEN = 0.5f;
 
+    //calling from scene on HomeScreen Game Object (Enabler script)
     public void OnHomePageEnable() {
         isHomePageActive = true;
         StartCoroutine(ShowBroadcastHoldingScreenWithDelay());
     }
 
+    //calling from scene on HomeScreen Game Object (Enabler script)
     public void OnHomePageDisable() {
         isHomePageActive = false;
         StopAllCoroutines();
     }
 
-    private void ShowBroadcastHoldingScreen() {
+    private void ShowContent() {
         HelperFunctions.DevLog("check room ShowBroadcastHoldingScreen");
+        HelperFunctions.DevLog("isHomePageActive " + isHomePageActive);
         if (isHomePageActive && contentLinkHandler.HasContentId(ContentLinkHandlerType.Room)) {
             SwitcherToRoomBroadcastHoldingScreen.Switch();
 
             HelperFunctions.DevLog("room ShowBroadcastHoldingScreen");
         } else if(isHomePageActive && contentLinkHandler.HasContentId(ContentLinkHandlerType.Stream)) {
             long id = 0;
+            HelperFunctions.DevLog("try parse PopContentId");
             if (!long.TryParse(contentLinkHandler.PopContentId(), out id))
                 return;
 
-            webRequestHandler.GetRequest(GetRequestStreamByIdURL(id),
-            (code, body) => {
-
-                StreamJsonData.Data streamJsonData = JsonUtility.FromJson<StreamJsonData.Data>(body);
-                if (streamJsonData == null) return;
-
-                if(isHomePageActive)
-                    pnlThumbnailPopup.OpenStream(streamJsonData);
-
-           },
-       (code, body) => { HelperFunctions.DevLog("Error DownloadStreamById " + id); },
-       accountManager.GetAccessToken().access);
+            if (isHomePageActive)
+                pnlThumbnailPopup.OpenStream(id);
         }
     }
 
-    private string GetRequestStreamByIdURL(long id) {
-        return webRequestHandler.ServerURLMediaAPI + videoUploader.StreamById.Replace("{id}", id.ToString());
-    }
-
     private void OnEnable() {
-        StreamCallBacks.onOpenRoom += ShowBroadcastHoldingScreen;
+        StreamCallBacks.onOpenRoom += ShowContent;
+        StreamCallBacks.onOpenStream += ShowContent;
     }
 
     private void OnDisable() {
-        StreamCallBacks.onOpenRoom -= ShowBroadcastHoldingScreen;
+        StreamCallBacks.onOpenRoom -= ShowContent;
+        StreamCallBacks.onOpenStream -= ShowContent;
     }
 
     private IEnumerator ShowBroadcastHoldingScreenWithDelay() {
         yield return new WaitForSeconds(DELAY_TO_SHOW_BROADCASTER_HOLDING_SCREEN);
 
         if (isHomePageActive)
-            ShowBroadcastHoldingScreen();
+            ShowContent();
     }
 }
