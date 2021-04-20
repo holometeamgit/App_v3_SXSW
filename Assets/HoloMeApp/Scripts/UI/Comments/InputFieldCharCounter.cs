312915df181a652dcd5bccacf7eefd2eab30a1ec
IvanKlimov1 <ivan.klimov@holo.me>
2021-04-20 18:07:43 +0300

    Add comment char limit

---
+++ Assets/HoloMeApp/Scripts/UI/Comments/InputFieldCharCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputFieldCharCounter : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField _inputField;
+
+    [SerializeField]
+    TMP_Text _txtCount;
+
+    public void UpdateCountCharacters(string text) {
+        _txtCount.text = text.Length + "/" + _inputField.characterLimit;
+    }
+}
