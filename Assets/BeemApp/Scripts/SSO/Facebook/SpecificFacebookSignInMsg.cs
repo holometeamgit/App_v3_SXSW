0ea84844073066b6bb7b6e86a57eca1a156f0cc7
Zuheb <zed_jay_1@hotmail.com>
2021-04-28 18:25:23 +0100

    Revert "Merge pull request #45 from holometeamgit/feature/DeleteFacebook"
    This reverts commit bd5a20aa03578d639877ff86bde99210d9ff2557, reversing
    changes made to 640fc49ad5f7b3bcd29b43ae5f17eaa637f17ced.

---
+++ Assets/BeemApp/Scripts/SSO/Facebook/SpecificFacebookSignInMsg.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecificFacebookSignInMsg : MonoBehaviour
+{
+    public static string Title = null;
+    public static string SpecificMsg = "Unfortunately you are unable to log in using your Facebook account at this time.\n\nPlease log in with the email address associated to your Facebook account and enter your password manually.";
+}
---
+++ Assets/HoloMeApp/Scripts/PanelScripts/PnlSignUpWelcome.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PnlSignUpWelcome : MonoBehaviour
+{
+    [SerializeField] FacebookAccountManager facebookAccountManager;
+    [SerializeField] PnlProfile pnlProfile;
+    [SerializeField] Switcher switcherToProfile;
+    [SerializeField] Switcher switcherToLogIn;
+    [SerializeField] Switcher switcherToSignUpEmail;
+
+
+    public void AppleSignUp() { }
+    public void GoogleSignUp() { }
+
+    public void FacebookSignUp() {
+        facebookAccountManager.SignUp();
+    }
+
+    public void OpenProfilePanel() {
+        if (!gameObject.activeInHierarchy)
+            return;
+        switcherToProfile.Switch();
+    }
+
+    public void OpenLogInPanel() {
+        switcherToLogIn.Switch();
+    }
+
+    public void OpenSignUpEmail() {
+        switcherToSignUpEmail.Switch();
+    }
+}
+    
