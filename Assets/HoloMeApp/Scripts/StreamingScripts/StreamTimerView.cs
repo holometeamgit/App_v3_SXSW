56b0184e06af5522b05ab769e98193bb2f0b327f
dsharabarov <dsharabarov@axmor.com>
2021-04-29 17:40:40 +0700

    streamtimerview

--- Assets/HoloMeApp/Scripts/StreamingScripts/StreamTimerView.cs
+++ Assets/HoloMeApp/Scripts/StreamingScripts/StreamTimerView.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,17 +13,29 @@ public class StreamTimerView : MonoBehaviour
     [SerializeField]
     private Text timerText;
 
+    private bool active;
+
     private void OnEnable()
     {
+        active = false;
         StreamCallBacks.onLiveStreamCreated += OnLiveStreamCreated;
+        StreamCallBacks.onLiveStreamFinished += OnLiveStreamFinish;
     }
 
     private void OnDisable()
     {
         StreamCallBacks.onLiveStreamCreated -= OnLiveStreamCreated;
+        StreamCallBacks.onLiveStreamFinished -= OnLiveStreamFinish;
     }
 
-    private void OnLiveStreamCreated(StreamStartResponseJsonData streamStartResponseJsonData) { 
+    private async void OnLiveStreamCreate(StreamStartResponseJsonData streamStartResponseJsonData) {
+        active = true;
+        while (active) {
+            await Task.Yield();
+        }
+    }
+
+    private void OnLiveStreamFinish() { 
     
     }
 
