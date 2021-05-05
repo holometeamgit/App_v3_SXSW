2321cfedc7b5b547abe64ff352ee74fdf6e677f6
IvanKlimov1 <ivan.klimov@holo.me>
2021-05-05 15:22:46 +0300

    update splashscreen and added MVP

---
+++ Assets/HoloMeApp/Scripts/Controllers/Preloader/SplashScreenConstructor.cs
@@ -0,0 +1,37 @@
+﻿//constructor for preloaderController in future it will be removed in DI
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beem {
+
+    public class SplashScreenConstructor : MonoBehaviour {
+        [SerializeField] PnlSplashScreen _pnlSplashScreen;
+        [SerializeField] GeneralAppAPIScriptableObject _generalAppAPIScriptableObject;
+        [SerializeField] WebRequestHandler _webRequestHandler;
+
+        private SplashScreenController _preloaderController;
+
+        private void Awake() {
+            VersionChecker versionChecker = new VersionChecker(_generalAppAPIScriptableObject, _webRequestHandler);
+            _preloaderController = new SplashScreenController(versionChecker);
+
+            _pnlSplashScreen.onViewEnabled += _preloaderController.Preload;
+            _pnlSplashScreen.onViewStartHide += _preloaderController.OnViewStartHide;
+
+            _preloaderController.onSentNeedUpdateApp += _pnlSplashScreen.ShowNeedUpdate;
+            _preloaderController.onSignIn += _pnlSplashScreen.HideSplashScreen;
+            _preloaderController.onFailSignIn += _pnlSplashScreen.AuthorisationErrorInvoke;
+        }
+
+        private void OnDestroy() {
+            _pnlSplashScreen.onViewEnabled -= _preloaderController.Preload;
+            _pnlSplashScreen.onViewStartHide -= _preloaderController.OnViewStartHide;
+
+            _preloaderController.onSentNeedUpdateApp -= _pnlSplashScreen.ShowNeedUpdate;
+            _preloaderController.onSignIn -= _pnlSplashScreen.HideSplashScreen;
+            _preloaderController.onFailSignIn -= _pnlSplashScreen.AuthorisationErrorInvoke;
+        }
+    }
+}
--- Assets/HoloMeApp/Scripts/Controllers/VersionChecker.cs
+++ Assets/HoloMeApp/Scripts/Controllers/VersionChecker.cs
@@ -1,90 +1,107 @@
-﻿using System.Collections;
+﻿//Check app version before app start
+//from v4 just force update requirements for app 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Threading.Tasks;
 
-public class VersionChecker : MonoBehaviour {
-    public Action OnNeedUpdateApp;
-    public Action OnCanUse;
+namespace Beem {
 
-    [SerializeField] GeneralAppAPIScriptableObject generalAppAPIScriptableObject;
-    [SerializeField] WebRequestHandler webRequestHandler;
-    [SerializeField] float delayRepeat = 2;
+    public class VersionChecker {
+        public Action onSentNeedUpdateApp;
+        public Action onSentCanUse;
 
-    private const string DEFAULT_MIN_VERSION = "0";
+        GeneralAppAPIScriptableObject _generalAppAPIScriptableObject;
+        WebRequestHandler _webRequestHandler;
 
-    public void RequestVersion() {
-        webRequestHandler.GetRequest(GetRequestURL(), VersionCallBack, ErrorVersionCallBack);
-    }
+        private const int DELAY_REPEAT = 2000;
+        private const string DEFAULT_MIN_VERSION = "0";
 
-    //v4 just force update
-    private void VersionCallBack(long code, string body) {
-
-        HelperFunctions.DevLog("VersionCallBack " + body);
-
-        AppVersionJsonData versionData = JsonParser.CreateFromJSON<AppVersionJsonData>(body);
-        if (versionData == null)
-            StartCoroutine(RepeatCheckVersion());
-
-        if (versionData.versions.Count == 0) {
-            OnCanUse?.Invoke();
-            return;
+        public VersionChecker(GeneralAppAPIScriptableObject generalAppAPIScriptableObject, WebRequestHandler webRequestHandler) {
+            _generalAppAPIScriptableObject = generalAppAPIScriptableObject;
+            _webRequestHandler = webRequestHandler;
         }
 
-        string currentMinVersionWithForceUpdate = DEFAULT_MIN_VERSION;
-        foreach (var version in versionData.versions) {
+        //[Inject]
+        public void Constructor(GeneralAppAPIScriptableObject generalAppAPIScriptableObject, WebRequestHandler webRequestHandler) {
+            _generalAppAPIScriptableObject = generalAppAPIScriptableObject;
+            _webRequestHandler = webRequestHandler;
+        }
+
+        public void RequestVersion() {
+            _webRequestHandler.Get(GetRequestURL(), VersionCallBack, ErrorVersionCallBack, false);
+        }
+
+        private void VersionCallBack(long code, string body) {
+
+            HelperFunctions.DevLog("VersionCallBack " + body);
+
+            AppVersionJsonData versionData = JsonParser.CreateFromJSON<AppVersionJsonData>(body);
+            if (versionData == null)
+                RepeatCheckVersion();
+
+            if (versionData.versions.Count == 0) {
+                onSentCanUse?.Invoke();
+                return;
+            }
+
+            string currentMinVersionWithForceUpdate = DEFAULT_MIN_VERSION;
+            foreach (var version in versionData.versions) {
 
 #if UNITY_IOS
-            if (version.platform != AppVersionJsonData.IOS_PLATFORM) {
-                continue;
-            }
+                if (version.platform != AppVersionJsonData.IOS_PLATFORM) {
+                    continue;
+                }
 #elif UNITY_ANDROID
             if (version.platform != AppVersionJsonData.ANDROID_PLATFORM) {
                 continue;
             }
 #endif
-            if (CompareVersions(version.min_support_version, currentMinVersionWithForceUpdate) > 0 && version.forced_update) {
-                currentMinVersionWithForceUpdate = version.min_support_version;
+                if (CompareVersions(version.min_support_version, currentMinVersionWithForceUpdate) > 0 && version.forced_update) {
+                    currentMinVersionWithForceUpdate = version.min_support_version;
+                }
             }
-        }
-        if (CompareVersions(currentMinVersionWithForceUpdate, Application.version) > 0)
-            OnNeedUpdateApp?.Invoke();
-        else
-            OnCanUse?.Invoke();
-    }
-
-    private void ErrorVersionCallBack(long code, string body) {
-        StartCoroutine(RepeatCheckVersion());
-    }
-
-    IEnumerator RepeatCheckVersion() {
-        yield return new WaitForSeconds(delayRepeat);
-        RequestVersion();
-    }
-
-    private int CompareVersions(string version1, string version2) {
-        string[] subversions1 = version1.Split('.');
-        string[] subversions2 = version2.Split('.');
-
-        int countSubversion = Mathf.Min(subversions1.Length, subversions2.Length);
-
-        for (int i = 0; i < countSubversion; i++) {
-            int version1Number = 0;
-            int version2Number = 0;
-
-            int.TryParse(subversions1[i], out version1Number);
-            int.TryParse(subversions2[i], out version2Number);
-            if (version1Number > version2Number) {
-                return 1;
-            } else if (version1Number < version2Number) {
-                return -1;
-            }
+            if (CompareVersions(currentMinVersionWithForceUpdate, Application.version) > 0)
+                onSentNeedUpdateApp?.Invoke();
+            else
+                onSentCanUse?.Invoke();
         }
 
-        return 0;
-    }
+        private void ErrorVersionCallBack(long code, string body) {
+            RepeatCheckVersion();
+        }
 
-    private string GetRequestURL() {
-        return webRequestHandler.ServerURLAuthAPI + generalAppAPIScriptableObject.Version;
+        private void RepeatCheckVersion() {
+            var taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            Task.Delay(DELAY_REPEAT).ContinueWith(_ => RequestVersion(), taskScheduler);
+        }
+
+        private int CompareVersions(string version1, string version2) {
+            string[] subversions1 = version1.Split('.');
+            string[] subversions2 = version2.Split('.');
+
+            int countSubversion = Mathf.Min(subversions1.Length, subversions2.Length);
+
+            for (int i = 0; i < countSubversion; i++) {
+                int version1Number = 0;
+                int version2Number = 0;
+
+                int.TryParse(subversions1[i], out version1Number);
+                int.TryParse(subversions2[i], out version2Number);
+                if (version1Number > version2Number) {
+                    return 1;
+                } else if (version1Number < version2Number) {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+
+        private string GetRequestURL() {
+            return _webRequestHandler.ServerURLAuthAPI + _generalAppAPIScriptableObject.Version;
+        }
     }
 }
