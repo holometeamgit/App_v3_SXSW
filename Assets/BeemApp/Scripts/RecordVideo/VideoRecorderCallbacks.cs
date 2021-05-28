cf8075cf395265bd00e2ee32152d86ed6d11369d
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-05-28 19:02:40 +0700

    VideoRecorder

---
+++ Assets/BeemApp/Scripts/RecordVideo/VideoRecorderCallbacks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoRecorderCallbacks {
+    public static Action onStartRecording = delegate { };
+    public static Action onStopRecording = delegate { };
+    public static Action onSnapshotStarted = delegate { };
+    public static Action onSnapshotEnded = delegate { };
+}
---
+++ Assets/BeemApp/Scripts/RecordVideo/VideoRecorderController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.IO;
+using NatCorder;
+using NatCorder.Clocks;
+using NatCorder.Inputs;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VideoRecorderController : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private bool recording { get; set; }
+    private bool recordLengthFailed;
+    private bool recordMicrophone = true;
+
+    private MP4Recorder videoRecorder;
+    private IClock recordingClock;
+    private CameraInput cameraInput;
+    private AudioInput audioInput;
+    private Coroutine currentCoroutine;
+    private PermissionGranter permissionGranter;
+    private Camera[] cameras;
+    private int videoWidth;
+    private int videoHeight;
+
+    private string lastRecordingPath;
+
+    enum Mode { Video, Photo };
+    private Mode mode;
+    private Mode ChangeMode {
+        set {
+            mode = value;
+        }
+    }
+
+    private void Awake() {
+        permissionGranter = FindObjectOfType<PermissionGranter>();
+        cameras = FindObjectsOfType<Camera>();
+    }
+
+    private void Start() {
+        CorrectResolutionAspect();
+        ChangeMode = Mode.Video;
+    }
+
+    private void CorrectResolutionAspect() {
+        videoWidth = MakeEven(Screen.width / 2);
+        videoHeight = MakeEven(Screen.height / 2);
+    }
+
+    public int MakeEven(int value) {
+        return value % 2 == 0 ? value : value - 1;
+    }
+
+    public void StartRecording() {
+        if (!permissionGranter.MicAccessAvailable) {
+            recordMicrophone = false;
+        }
+        recordLengthFailed = false;
+        recordingClock = new RealtimeClock();
+        videoRecorder = new MP4Recorder(
+            videoWidth,
+            videoHeight,
+            25,
+            recordMicrophone ? AudioSettings.outputSampleRate : 0,
+            recordMicrophone ? (int)AudioSettings.speakerMode : 0,
+            OnRecordComplete
+        );
+
+        cameraInput = new CameraInput(videoRecorder, recordingClock, cameras);
+        if (recordMicrophone) {
+            audioInput = new AudioInput(videoRecorder, recordingClock, audioSource);
+        }
+
+        recording = true;
+
+        VideoRecorderCallbacks.onStartRecording?.Invoke();
+    }
+
+    public void RecordLengthFail() {
+        VideoRecorderCallbacks.onStopRecording?.Invoke();
+        MakeScreenshot();
+        recordLengthFailed = true;
+    }
+
+    public void StopRecording() {
+        if (recordMicrophone) {
+            audioInput.Dispose();
+        }
+
+        cameraInput.Dispose();
+        videoRecorder.Dispose();
+        recording = false;
+    }
+
+    private void OnRecordComplete(string outputPath) {
+        if (recordLengthFailed) {
+            File.Delete(outputPath);
+            MakeScreenshot();
+        } else {
+            lastRecordingPath = outputPath;
+        }
+        VideoRecorderCallbacks.onStopRecording?.Invoke();
+    }
+
+    private void MakeScreenshot() {
+        if (currentCoroutine == null) {
+            VideoRecorderCallbacks.onSnapshotStarted?.Invoke();
+            currentCoroutine = StartCoroutine(ScreenShotAsync());
+        }
+    }
+
+    private IEnumerator ScreenShotAsync() {
+
+        yield return new WaitForEndOfFrame();
+        Texture2D screenShot = ScreenCapture.CaptureScreenshotAsTexture(1);
+        yield return new WaitForEndOfFrame();
+        Sprite spr = Sprite.Create(screenShot, new Rect(0, 0, Screen.width, Screen.height), Vector2.one * 0.5f);
+        VideoRecorderCallbacks.onSnapshotEnded?.Invoke();
+        currentCoroutine = null;
+    }
+}
