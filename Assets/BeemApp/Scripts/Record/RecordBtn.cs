af9142d8dd36812ff957e3fbe4285940cf966e47
dsharabarov <dsharabarov@axmor.com>
2021-06-03 12:09:35 +0700

    Fix namespace

--- Assets/BeemApp/Scripts/Record/RecordBtn.cs
+++ Assets/BeemApp/Scripts/Record/RecordBtn.cs
@@ -1,71 +1,76 @@
-﻿using System;
+﻿using Beem.Record.Video;
+using System;
 using System.Collections;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+namespace Beem.Record {
+    /// <summary>
+    /// Record Button
+    /// </summary>
+    public class RecordBtn : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
-public class RecordBtn : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+        [Header("Recording Time")]
+        [SerializeField]
+        private Vector2 recordingTime = new Vector2(2, 15);
 
-    [Header("Recording Time")]
-    [SerializeField]
-    private Vector2 recordingTime = new Vector2(2, 15);
+        private float timer = 0;
 
-    private float timer = 0;
-
-    private float Timer {
-        get {
-            return timer;
-        }
-        set {
-            if (Mathf.Abs(timer - value) > Mathf.Epsilon) {
-                timer = value;
-                VideoRecordCallbacks.onRecordProgress?.Invoke(timer / (recordingTime.y));
+        private float Timer {
+            get {
+                return timer;
+            }
+            set {
+                if (Mathf.Abs(timer - value) > Mathf.Epsilon) {
+                    timer = value;
+                    VideoRecordCallbacks.onRecordProgress?.Invoke(timer / (recordingTime.y));
+                }
             }
         }
-    }
 
-    private CancellationTokenSource cancelTokenSource;
-    private bool pressed = false;
+        private CancellationTokenSource cancelTokenSource;
+        private bool pressed = false;
 
-    public void OnPointerDown(PointerEventData eventData) {
-        pressed = true;
-        Record();
-    }
+        public void OnPointerDown(PointerEventData eventData) {
+            pressed = true;
+            Record();
+        }
 
-    public void OnPointerUp(PointerEventData eventData) {
-        pressed = false;
-    }
+        public void OnPointerUp(PointerEventData eventData) {
+            pressed = false;
+        }
 
-    public async void Record() {
-        cancelTokenSource = new CancellationTokenSource();
-        try {
-            VideoRecordCallbacks.onRecordStarted?.Invoke();
-            Timer = 0;
-            float startTime = Time.time;
-            while (Timer < recordingTime.y && pressed) {
-                Timer = Time.time - startTime;
-                await Task.Yield();
+        public async void Record() {
+            cancelTokenSource = new CancellationTokenSource();
+            try {
+                VideoRecordCallbacks.onRecordStarted?.Invoke();
+                Timer = 0;
+                float startTime = Time.time;
+                while (Timer < recordingTime.y && pressed) {
+                    Timer = Time.time - startTime;
+                    await Task.Yield();
+                }
+                if (Timer < recordingTime.x) {
+                    VideoRecordCallbacks.onRecordFailed?.Invoke();
+                } else {
+                    VideoRecordCallbacks.onRecordStoped?.Invoke();
+                }
+                Timer = 0;
+            } finally {
+                if (cancelTokenSource != null) {
+                    cancelTokenSource.Dispose();
+                    cancelTokenSource = null;
+                }
             }
-            if (Timer < recordingTime.x) {
-                VideoRecordCallbacks.onRecordFailed?.Invoke();
-            } else {
-                VideoRecordCallbacks.onRecordStoped?.Invoke();
-            }
-            Timer = 0;
-        } finally {
+        }
+
+        public void Cancel() {
             if (cancelTokenSource != null) {
-                cancelTokenSource.Dispose();
+                cancelTokenSource.Cancel();
                 cancelTokenSource = null;
             }
-        }
-    }
-
-    public void Cancel() {
-        if (cancelTokenSource != null) {
-            cancelTokenSource.Cancel();
-            cancelTokenSource = null;
         }
     }
 }
--- Assets/BeemApp/Scripts/VideoPlayer/VideoPlayerSearcher.cs
+++ Assets/BeemApp/Scripts/VideoPlayer/VideoPlayerSearcher.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
+﻿using Beem.Record.Video;
+using Beem.Video;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
-namespace Beem.Video {
-    /// <summary>
-    /// VideoPlayer Searcher
-    /// </summary>
-    public class VideoPlayerSearcher : MonoBehaviour {
 
-        private void OnEnable() {
-            VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
-            AudioSource audioSourcer = GetComponent<AudioSource>();
-            VideoPlayerCallBacks.onSetVideoPlayer?.Invoke(videoPlayer);
-            VideoRecordCallbacks.onSetAudioSource?.Invoke(audioSourcer);
-        }
+/// <summary>
+/// VideoPlayer Searcher
+/// </summary>
+public class VideoPlayerSearcher : MonoBehaviour {
 
+    private void OnEnable() {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        AudioSource audioSourcer = GetComponent<AudioSource>();
+        VideoPlayerCallBacks.onSetVideoPlayer?.Invoke(videoPlayer);
+        VideoRecordCallbacks.onSetAudioSource?.Invoke(audioSourcer);
     }
+
 }
+
