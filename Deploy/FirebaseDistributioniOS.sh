2ed8027232d3796c9db1ac31e83e4a1ac2202df7
dsharabarov <dsharabarov@axmor.com>
2021-06-16 15:15:54 +0700

    Fix Firebase Distribution data

--- Deploy/FirebaseDistributioniOS.sh
+++ Deploy/FirebaseDistributioniOS.sh
@@ -3,6 +3,14 @@
 
 echo "Uploading IPA to Appstore Connect..."
 
-path = "WORKSPACE/.build/last/$TARGET_NAME/build.ipa"
+#Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
+path="$WORKSPACE/.build/last/$TARGET_NAME/build.ipa"
+release="Firebase Distribution Test"
+token="1//0cVIYMiei3SyGCgYIARAAGAwSNwF-L9IrRwas9jSXkAepwnuw51TfbFehS0Iq7w9SZ-Q4CkUsWY9fF9TflH1Nx1YB6EHGzcaqEbg"
+group="Test"
+firebaseApp="1:233061171188:ios:1c742b53c13c922fcf43a1";
 
-(npm bin)/firebase appdistribution:distribute $path --app 1:233061171188:ios:1c742b53c13c922fcf43a1 --release-notes "Firebase Distribution Test" --groups "Test"
+
+npm install -g firebase-tools
+
+firebase appdistribution:distribute $path --app $firebaseApp --release-notes $release --groups $group --token $token;
