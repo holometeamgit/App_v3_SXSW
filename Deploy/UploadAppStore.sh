be5b59a08c8aaf7c0fa83994ba3817a4f76a9bff
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-06-18 15:46:35 +0700

    hot fix

--- Deploy/UploadAppStore.sh
+++ Deploy/UploadAppStore.sh
@@ -10,7 +10,7 @@ if [ -z "$FIREBASE_BUILD" ]; then
     echo "Could not find .ipa/.apk file"
     exit 1
 else
-    if xcrun altool --upload-app -f $$FIREBASE_BUILD -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
+    if xcrun altool --upload-app -f $FIREBASE_BUILD -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
         echo "Upload IPA to Appstore Connect finished with success"
     else
         echo "Upload IPA to Appstore Connect failed"
