05ff0190e840fd33a92cc8602027a64f82b35e90
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-06-18 13:38:00 +0700

    deploy for firebase tools

--- Deploy/FirebaseDistribution.sh
+++ Deploy/FirebaseDistribution.sh
@@ -10,6 +10,8 @@ if [ -z "$FIREBASE_BUILD" ]; then
     echo "Could not find .ipa/.apk file"
     exit 1
 else
+    echo "Install Firebase Tools.."
+    npm install -g firebase-tools
     echo "Firebase Distribution..."
     firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
 fi
