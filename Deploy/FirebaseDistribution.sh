e676c7b23328b03611ab6577ea427934309d9d9a
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-06-18 12:37:18 +0700

    Test Build

--- Deploy/FirebaseDistribution.sh
+++ Deploy/FirebaseDistribution.sh
@@ -3,22 +3,13 @@
 
 echo "Uploading Build Result to Firebase Distribution..."
 
-#Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
-#path="$WORKSPACE/.build/last/$BUILD_TARGET_ID/build.ipa"
-
 set -x
 
 export FIREBASE_BUILD="$(find -E . -regex '.*\.(ipa|apk)' -print -quit)"
 if [ -z "$FIREBASE_BUILD" ]; then
-  echo "Could not find .ipa/.apk file"
-  exit 1
+    echo "Could not find .ipa/.apk file"
+    exit 1
 else
-
-#echo "Installing npm..."
-
-#npm install -g firebase-tools
-
-echo "Firebase Distribution..."
-
-firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
+    echo "Firebase Distribution..."
+    firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
 fi
--- Deploy/UploadAppStore.sh
+++ Deploy/UploadAppStore.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
  
 echo "Uploading IPA to Appstore Connect..."
- 
-#Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
-path="$WORKSPACE/.build/last/$BUILD_TARGET_ID/build.ipa"
- 
-if xcrun altool --upload-app -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
-    echo "Upload IPA to Appstore Connect finished with success"
+
+set -x
+
+export FIREBASE_BUILD="$(find -E . -regex '.*\.(ipa|apk)' -print -quit)"
+
+if [ -z "$FIREBASE_BUILD" ]; then
+    echo "Could not find .ipa/.apk file"
+    exit 1
 else
-    echo "Upload IPA to Appstore Connect failed"
+    if xcrun altool --upload-app -f $$FIREBASE_BUILD -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
+        echo "Upload IPA to Appstore Connect finished with success"
+    else
+        echo "Upload IPA to Appstore Connect failed"
+    fi
 fi
