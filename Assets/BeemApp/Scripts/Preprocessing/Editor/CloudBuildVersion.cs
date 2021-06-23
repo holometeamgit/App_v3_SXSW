b1707545340c518c074318fee0b71d999623531e
dsharabarov <dsharabarov@axmor.com>
2021-06-23 12:30:38 +0700

    Test Cloud Build Version

--- Assets/BeemApp/Scripts/Preprocessing/Editor/CloudBuildVersion.cs
+++ Assets/BeemApp/Scripts/Preprocessing/Editor/CloudBuildVersion.cs
@@ -7,45 +7,21 @@ using UnityEngine;
 /// <summary>
 /// Preprocessing for Build Version
 /// </summary>
-public class CloudBuildVersion : IPreprocessBuild {
-
-    private static string _versionNumber;
-    private static string _buildNumber;
-
-    public int callbackOrder { get { return 0; } }
-    public void OnPreprocessBuild(BuildTarget target, string path) {
-        _versionNumber = Environment.GetEnvironmentVariable("VERSION_NUMBER");
-
-        _buildNumber = Environment.GetEnvironmentVariable("BUILD_NUMBER");
-
-        Debug.Log("Preprocessing...");
-        if (!string.IsNullOrEmpty(_versionNumber)) {
-            PlayerSettings.bundleVersion = _versionNumber;
-        }
-
-        Debug.Log("_versionNumber = " + _versionNumber);
-
-        if (string.IsNullOrEmpty(_buildNumber)) {
-            if (target == BuildTarget.iOS) {
-                PlayerSettings.iOS.buildNumber = _buildNumber;
-            } else if (target == BuildTarget.Android) {
-                PlayerSettings.Android.bundleVersionCode = int.Parse(_buildNumber);
-            }
-        }
-
-        Debug.Log("_buildNumber = " + _buildNumber);
-    }
+public class CloudBuildVersion {
 
 #if UNITY_CLOUD_BUILD
-    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
-    {
+    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest) {
         string buildNumber = manifest.GetValue("buildNumber", "0");
 
         Debug.LogWarning("Setting build number to " + buildNumber);
-        PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
-        PlayerSettings.iOS.buildNumber = buildNumber;
-        Debug.Log("PlayerSettings.iOS.buildNumber = "+PlayerSettings.iOS.buildNumber);
-        Debug.Log("PlayerSettings.Android.bundleVersionCode = "+PlayerSettings.Android.bundleVersionCode);
+
+        if (string.IsNullOrEmpty(buildNumber)) {
+            PlayerSettings.iOS.buildNumber = buildNumber;
+            PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
+        }
+
+        Debug.Log("PlayerSettings.iOS.buildNumber = " + PlayerSettings.iOS.buildNumber);
+        Debug.Log("PlayerSettings.Android.bundleVersionCode = " + PlayerSettings.Android.bundleVersionCode);
     }
 #endif
 }
