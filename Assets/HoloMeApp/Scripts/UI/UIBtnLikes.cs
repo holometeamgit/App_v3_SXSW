2b492b34b5c4af051a79fb79f5ff5e303f4e9dd8
Sharabarov, Danil I <dsharabarov@axmor.com>
2021-07-15 14:45:08 +0700

    Update likes state on Init

--- Assets/HoloMeApp/Scripts/UI/UIBtnLikes.cs
+++ Assets/HoloMeApp/Scripts/UI/UIBtnLikes.cs
@@ -37,6 +37,7 @@ namespace Beem.UI {
             _streamId = streamId;
             CallBacks.onGetLikeStateCallBack += UpdateState;
             GetLikesState();
+            UpdateBtnUIState();
         }
 
         /// <summary>
