c0075c1b8c26cfe0c71c2d1d2b4635b2744b68a8
dsharabarov <dsharabarov@axmor.com>
2021-07-20 14:33:13 +0700

    Add defines for iOS

--- Assets/BeemApp/Scripts/Preprocessing/Editor/CloudBuildVersion.cs
+++ Assets/BeemApp/Scripts/Preprocessing/Editor/CloudBuildVersion.cs
@@ -12,18 +12,47 @@ public class CloudBuildVersionpublic : IPreprocessBuild {
 
     private static string _versionNumber;
     private static string _buildNumber;
+    private static string _buildType;
 
     private const string VERSION = "BEEM_VERSION";
-    private const string BUILD = "BEEM_BUILD";
+    private const string BUILD_NUMBER = "BEEM_BUILD";
+    private const string BUILD_TYPE = "BEEM_BUILD_TYPE";
+
+    private const string APPLICATION_NAME_DEV = "Beem Dev";
+    private const string APPLICATION_NAME = "Beem";
 
     private const string PROD = "PROD";
     private const string DEV = "DEV";
+
+    private string[] iOSDevDefines = new string[] {
+        "UNITY_XR_ARKIT_LOADER_ENABLED",
+        "CT_BWF",
+        DEV
+    };
+
+    private string[] iOSProdDefines = new string[] {
+        "UNITY_XR_ARKIT_LOADER_ENABLED",
+        "CT_BWF",
+        PROD
+    };
+
+    private string[] AndroidDevDefines = new string[] {
+        "CT_BWF",
+        DEV
+    };
+
+    private string[] AndroidProdDefines = new string[] {
+        "CT_BWF",
+        PROD
+    };
 
     public int callbackOrder { get { return 0; } }
     public void OnPreprocessBuild(BuildTarget target, string path) {
         _versionNumber = Environment.GetEnvironmentVariable(VERSION);
 
-        _buildNumber = Environment.GetEnvironmentVariable(BUILD);
+        _buildNumber = Environment.GetEnvironmentVariable(BUILD_NUMBER);
+
+        _buildType = Environment.GetEnvironmentVariable(BUILD_TYPE);
 
         PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, EditorUserBuildSettings.development ? DEV : PROD);
         PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, EditorUserBuildSettings.development ? DEV : PROD);
@@ -49,6 +78,13 @@ public class CloudBuildVersionpublic : IPreprocessBuild {
             if (int.Parse(_buildNumber) > PlayerSettings.Android.bundleVersionCode) {
                 PlayerSettings.Android.bundleVersionCode = int.Parse(_buildNumber);
             }
+        }
+
+        if (!string.IsNullOrEmpty(_buildType)) {
+            PlayerSettings.productName = _buildType == DEV ? APPLICATION_NAME_DEV : APPLICATION_NAME;
+            EditorUserBuildSettings.development = _buildType == DEV;
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, _buildType == DEV ? iOSDevDefines : iOSProdDefines);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, _buildType == DEV ? AndroidDevDefines : AndroidProdDefines);
         }
     }
 
