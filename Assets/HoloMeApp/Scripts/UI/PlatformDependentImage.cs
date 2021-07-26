97d64d64e37ed231ae0bd6260c4685cbd25b3ffa
IvanKlimov1 <ivan.klimov@holo.me>
2021-07-26 22:01:31 +0300

    Make Platform dependent Images for stars on onboarding screen

---
+++ Assets/HoloMeApp/Scripts/UI/PlatformDependentImage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class PlatformDependentImage : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite _appleImage;
+    [SerializeField]
+    private Sprite _androidImage;
+    private Image _image;
+
+    private void Awake() {
+        if (_image == null)
+            _image = GetComponent<Image>();
+#if UNITY_IOS
+        _image.sprite = _appleImage;
+#else
+        _image.sprite = _androidImage;
+#endif
+    }
+}
