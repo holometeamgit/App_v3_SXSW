7710d9d57f144a35267847df62612834cc41d91e
IvanKlimov1 <ivan.klimov@holo.me>
2021-08-14 15:46:22 +0300

    Add abbreviation for likes

--- Assets/HoloMeApp/Scripts/Helper/DataStringConverter.cs
+++ Assets/HoloMeApp/Scripts/Helper/DataStringConverter.cs
@@ -6,7 +6,11 @@ public static class DataStringConverter {
     /// convert long number to items singular or plural style
     /// exemple: 1 view or 2 views
     /// </summary>
-    public static string GetItems(long countViews, string singularValue, string pluralValue) {
+    public static string GetItems(long countViews, string singularValue, string pluralValue, bool isShowZero = false, string zeroValue = "") {
+        if(isShowZero && countViews == 0)
+            return countViews + " " + zeroValue;
+
+
         return countViews < 1 ? "" : NumberToKMBConverter.ToKMB(countViews) + " " +
             (countViews == 1 ? singularValue : pluralValue);
     }
