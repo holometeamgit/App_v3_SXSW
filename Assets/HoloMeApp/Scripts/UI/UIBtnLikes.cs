189fd3f78e5954d091dbe5d4ac1e1a1d3d2fa8e1
IvanKlimov1 <ivan.klimov@holo.me>
2021-08-27 15:19:16 +0300

    hide 0 like + update thumbnail layout

--- Assets/HoloMeApp/Scripts/UI/UIBtnLikes.cs
+++ Assets/HoloMeApp/Scripts/UI/UIBtnLikes.cs
@@ -82,7 +82,7 @@ namespace Beem.UI {
         private void UpdateBtnUIState() {
             imageLike.enabled = _isLike;
             imageUnlike.enabled = !_isLike;
-            likesCount.text = DataStringConverter.GetItems(_count, "", "", true);
+            likesCount.text = _count < 1 ? "" : DataStringConverter.GetItems(_count, "", "", true);
         }
 
         private void GetLikesState() {
