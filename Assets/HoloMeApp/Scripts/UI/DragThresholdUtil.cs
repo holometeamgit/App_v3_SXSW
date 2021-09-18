9246893b1e37e707127ce2e501d452d4a9453dbf
IvanKlimov1 <ivan.klimov@holo.me>
2021-09-18 22:31:01 +0300

    fixed Overlay hologram placement and home screens

---
+++ Assets/HoloMeApp/Scripts/UI/DragThresholdUtil.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragThresholdUtil : MonoBehaviour {
+    private const float MIDDLE_VALUE_DPI = 160f;
+
+    void Start() {
+        int defaultValue = EventSystem.current.pixelDragThreshold;
+        EventSystem.current.pixelDragThreshold =
+                Mathf.Max(
+                     defaultValue,
+                     (int)(defaultValue * Screen.dpi / MIDDLE_VALUE_DPI));
+    }
+}
