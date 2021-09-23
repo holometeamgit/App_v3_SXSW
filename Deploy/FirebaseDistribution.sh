2a5fe1b8d5672c368ed5fd1f9aa02051ab27903f
dsharabarov <dsharabarov@axmor.com>
2021-09-23 14:04:20 +0700

    Add build json

--- Deploy/FirebaseDistribution.sh
+++ Deploy/FirebaseDistribution.sh
@@ -5,6 +5,16 @@ echo "Uploading Build Result to Firebase Distribution..."
 
 set -x
 
+build_json = $(jq . < build.json)
+
+build_target=$(jq -r 'keys[0]' < build.json)
+
+build_platform=$(jq -r ".[\"${build_target}\"].platform" < build.json)
+
+echo "Building $build_target for $build_platform"
+
+echo "Build Json $build_json" 
+
 export FIREBASE_BUILD="$(find -E . -regex '.*\.(ipa|apk|aab)' -print -quit)"
 if [ -z "$FIREBASE_BUILD" ]; then
     echo "Could not find .ipa/.apk/.aab file"
@@ -15,9 +25,3 @@ else
     echo "Firebase Distribution..."
     firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
 fi
-
-build_target=$(jq -r 'keys[0]' < build.json)
-
-build_platform=$(jq -r ".[\"${build_target}\"].platform" < build.json)
-
-echo "Building $build_target for $build_platform"
