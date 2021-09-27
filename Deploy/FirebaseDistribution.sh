55e85165fe12665b352485ceb2e44f6f0f6c4054
dsharabarov <dsharabarov@axmor.com>
2021-09-27 15:56:39 +0700

    Test load firebase tools

--- Deploy/FirebaseDistribution.sh
+++ Deploy/FirebaseDistribution.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 echo "Uploading Build Result to Firebase Distribution..."
@@ -6,12 +5,15 @@ echo "Uploading Build Result to Firebase Distribution..."
 set -x
 
 export FIREBASE_BUILD="$(find -E . -regex '.*\.(ipa|apk|aab)' -print -quit)"
+
+echo $FIREBASE_RELEASE_NOTES
+
 if [ -z "$FIREBASE_BUILD" ]; then
     echo "Could not find .ipa/.apk/.aab file"
     exit 1
 else
     echo "Install Firebase Tools.."
-    npm install -g firebase-tools
+    curl -Lo ./firebase_bin https://firebase.tools/bin/linux/latest
     echo "Firebase Distribution..."
     firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
 fi
