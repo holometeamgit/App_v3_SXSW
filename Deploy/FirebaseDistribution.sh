8a0c59e87a4a298c0917c3e21876b40e462b3115
dsharabarov <dsharabarov@axmor.com>
2021-09-29 15:42:24 +0700

    Fix link

--- Deploy/FirebaseDistribution.sh
+++ Deploy/FirebaseDistribution.sh
@@ -10,7 +10,7 @@ export BUILD_MANIFEST = "$(find -E . -regex '.*\UnityCloudBuildManifest.json)' -
 
 echo $BUILD_MANIFEST
 
-export BUILD_VALUE = "$(jq .<build.json)
+export BUILD_VALUE = "$(jq .<build.json)"
 
 echo $BUILD_VALUE
 
@@ -21,5 +21,5 @@ else
     echo "Install Firebase Tools.."
     npm install -g firebase-tools
     echo "Firebase Distribution..."
-    firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes-file $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
+    firebase appdistribution:distribute $FIREBASE_BUILD --app $FIREBASE_APP --release-notes $FIREBASE_RELEASE_NOTES --groups $FIREBASE_GROUPS --token $FIREBASE_TOKEN;
 fi
