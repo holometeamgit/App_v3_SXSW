41492eae95cd9b118ccb7c5aaac967640b7401cc
dsharabarov <dsharabarov@axmor.com>
2021-10-27 15:15:10 +0700

    Test

--- Assets/3rd-Party/CustomKeyBoard/KeyBoard/KeyBoardAction.cs
+++ Assets/3rd-Party/CustomKeyBoard/KeyBoard/KeyBoardAction.cs
@@ -1,13 +1,20 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Beem.KeyBoard {
     /// <summary>
     /// KeyBoard Actions
     /// </summary>
     public class KeyBoardAction : MonoBehaviour {
+
+        [SerializeField]
+        private InputField.OnChangeEvent onChangeEvent;
+        [SerializeField]
+        private InputField.SubmitEvent submitEvent;
+
         public void OpenKeyBoard(bool isOpened) {
-            KeyBoardConstructor.onShow?.Invoke(isOpened);
+            KeyBoardConstructor.onShow?.Invoke(isOpened, onChangeEvent, submitEvent);
         }
     }
 }
--- Assets/3rd-Party/CustomKeyBoard/KeyBoard/KeyBoardConstructor.cs
+++ Assets/3rd-Party/CustomKeyBoard/KeyBoard/KeyBoardConstructor.cs
@@ -2,6 +2,7 @@ using Mopsicus.Plugins;
 using NiceJson;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Beem.KeyBoard {
     /// <summary>
@@ -15,43 +16,114 @@ namespace Beem.KeyBoard {
         [SerializeField]
         private KeyBoardPositionView _positionSettingsView;
 
-        public static Action<bool> onShow = delegate { };
+        public static Action<bool, InputField.OnChangeEvent, InputField.SubmitEvent> onShow = delegate { };
+        public static Action<bool, InputField> onInputShow = delegate { };
 
-        private const string DATA = "data";
-        private const string HEIGHT = "height";
-        private const string SHOW = "show";
-
+        private InputField _currentInputField;
+        private InputSettings _inputSettings;
+        private InputField.OnChangeEvent _currentOnChangeEvent;
+        private InputField.SubmitEvent _currentSubmitEvent;
 
         private void Awake() {
             Construct();
         }
 
         private void Construct() {
-            Plugins.onJsonInit += Init;
             onShow += Show;
+            onInputShow += InputShow;
         }
 
-        private void Show(bool isShown) {
-            _mobileInputField.SetFocus(isShown);
+        private void Show(bool isShown, InputField.OnChangeEvent onChangeEvent, InputField.SubmitEvent submitEvent) {
             _keyboardField.SetActive(isShown);
+
+            _mobileInputField.SetFocus(isShown);
+
             _positionSettingsView.UpdatePosition();
+
+            if (!isShown) {
+                _mobileInputField.InputField.text = string.Empty;
+                if (_currentInputField != null) {
+                    _mobileInputField.InputField.onValueChanged.RemoveListener(ValueChanged);
+                    _mobileInputField.InputField.contentType = _inputSettings.contentType;
+                    _mobileInputField.InputField.inputType = _inputSettings.inputType;
+                    _mobileInputField.InputField.keyboardType = _inputSettings.keyboardType;
+                    _mobileInputField.InputField.characterValidation = _inputSettings.characterValidation;
+                    _mobileInputField.InputField.characterLimit = _inputSettings.characterLimit;
+                    _currentInputField = null;
+                }
+                _mobileInputField.InputField.text = string.Empty;
+            } else {
+                if (_currentOnChangeEvent != null) {
+                    _mobileInputField.InputField.onValueChanged.RemoveListener(EventChanged);
+                }
+                if (_currentSubmitEvent != null) {
+                    _mobileInputField.InputField.onEndEdit.RemoveListener(EventSubmit);
+                }
+                _currentOnChangeEvent = onChangeEvent;
+                _currentSubmitEvent = submitEvent;
+
+                if (_currentOnChangeEvent != null) {
+                    _mobileInputField.InputField.onValueChanged.AddListener(EventChanged);
+                }
+                if (_currentSubmitEvent != null) {
+                    _mobileInputField.InputField.onEndEdit.AddListener(EventSubmit);
+                }
+            }
+        }
+
+        private void InputShow(bool isShown, InputField inputField) {
+
+            Show(isShown, inputField.onValueChanged, inputField.onEndEdit);
+
+            if (inputField != null && isShown) {
+                _currentInputField = inputField;
+                _inputSettings = new InputSettings(_mobileInputField.InputField);
+                _mobileInputField.InputField.contentType = _currentInputField.contentType;
+                _mobileInputField.InputField.inputType = _currentInputField.inputType;
+                _mobileInputField.InputField.keyboardType = _currentInputField.keyboardType;
+                _mobileInputField.InputField.characterValidation = _currentInputField.characterValidation;
+                _mobileInputField.InputField.text = _currentInputField.text;
+                _mobileInputField.InputField.characterLimit = _currentInputField.characterLimit;
+                _mobileInputField.InputField.onValueChanged.AddListener(ValueChanged);
+            }
+        }
+
+        private void ValueChanged(string text) {
+            if (!string.IsNullOrEmpty(text)) {
+                _currentInputField.text = text;
+            }
+        }
+
+        private void EventChanged(string text) {
+            _currentOnChangeEvent?.Invoke(text);
+        }
+
+        private void EventSubmit(string text) {
+            _currentSubmitEvent?.Invoke(text);
         }
 
         private void OnDestroy() {
-            Plugins.onJsonInit -= Init;
             onShow -= Show;
+            onInputShow -= InputShow;
         }
 
-        private void Init(JsonObject data) {
-            string dataParse = data[DATA].ToString();
-            /*if (dataParse.Contains(HEIGHT)) {
-                JsonObject info = (JsonObject)JsonNode.ParseJsonString(dataParse);
-                _positionSettingsView.UpdatePosition(int.Parse(info[HEIGHT].ToString()));
+        /// <summary>
+        /// structure for previous input Settings
+        /// </summary>
+        public struct InputSettings {
+            public InputField.ContentType contentType { private set; get; }
+            public InputField.InputType inputType { private set; get; }
+            public TouchScreenKeyboardType keyboardType { private set; get; }
+            public InputField.CharacterValidation characterValidation { private set; get; }
+            public int characterLimit { private set; get; }
+
+            public InputSettings(InputField inputField) {
+                contentType = inputField.contentType;
+                inputType = inputField.inputType;
+                keyboardType = inputField.keyboardType;
+                characterValidation = inputField.characterValidation;
+                characterLimit = inputField.characterLimit;
             }
-            if (dataParse.Contains(SHOW)) {
-                JsonObject info = (JsonObject)JsonNode.ParseJsonString(dataParse);
-                Show((bool)info[SHOW]);
-            }*/
         }
     }
 }
