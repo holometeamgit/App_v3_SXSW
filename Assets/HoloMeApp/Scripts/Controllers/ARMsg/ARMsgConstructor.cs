bf1cb54ac964f23d5ad24d872c095f6f77c2bac4
dsharabarov <dsharabarov@axmor.com>
2021-12-01 14:37:30 +0700

    Add ar message if user doesn't have access token

--- Assets/HoloMeApp/Scripts/Controllers/ARMsg/ARMsgConstructor.cs
+++ Assets/HoloMeApp/Scripts/Controllers/ARMsg/ARMsgConstructor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System;
 using Beem.ARMsg;
 using Beem.Permissions;
+using System.Threading.Tasks;
+using System.Threading;
 
 public class ARMsgConstructor : MonoBehaviour {
     [SerializeField]
@@ -19,11 +21,16 @@ public class ARMsgConstructor : MonoBehaviour {
     [SerializeField]
     private PnlViewingExperience _pnlViewingExperience;
     [SerializeField]
+    private PopupShowChecker _popupShowChecker;
+    [SerializeField]
     private Switcher switchToHome;
 
     private ARMsgController _arMsgController;
+    private CancellationTokenSource _showCancellationTokenSource;
+    private CancellationToken _showCancellationToken;
 
     private ARMsgJSON.Data currentData;
+    private const int CHECK_COOLDOWN = 5000;
 
 
     private void Activate(ARMsgJSON.Data data) {
@@ -32,6 +39,10 @@ public class ARMsgConstructor : MonoBehaviour {
             return;
         }
 
+        OnReceivedRoomData(data, ActivateData);
+    }
+
+    private void ActivateData(ARMsgJSON.Data data) {
         currentData = data;
 
         _pnlViewingExperience.ActivateForARMessaging(data);
@@ -39,6 +50,39 @@ public class ARMsgConstructor : MonoBehaviour {
         _pnlARMessages.Init(data);
 
         _uiThumbnailsController.OnPlayFromUser?.Invoke(data.user);
+    }
+
+    private async Task WaitForCanShow() {
+        while (!_popupShowChecker.CanShow()) {
+            if (_showCancellationToken.IsCancellationRequested) {
+                _showCancellationToken.ThrowIfCancellationRequested();
+            }
+            await Task.Delay(CHECK_COOLDOWN);
+        }
+    }
+
+    private void OnReceivedRoomData(ARMsgJSON.Data data, Action<ARMsgJSON.Data> onSuccessTask) {
+        if (_showCancellationTokenSource != null) {
+            _showCancellationTokenSource.Cancel();
+            _showCancellationTokenSource.Dispose();
+        }
+
+        _showCancellationTokenSource = new CancellationTokenSource();
+        _showCancellationToken = _showCancellationTokenSource.Token;
+
+
+        TaskScheduler taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        WaitForCanShow().ContinueWith((task) => {
+
+            if (task.IsCanceled) {
+                HelperFunctions.DevLog("Previouses room deeplink request was interrupted");
+
+            } else if (task.IsCompleted) {
+
+                onSuccessTask?.Invoke(data);
+
+            }
+        }, taskScheduler);
     }
 
     private void Deactivate() {
--- Assets/HoloMeApp/Scripts/PanelScripts/PnlStreamPopup/PnlRoomPopupController.cs
+++ Assets/HoloMeApp/Scripts/PanelScripts/PnlStreamPopup/PnlRoomPopupController.cs
@@ -9,7 +9,7 @@ using System;
 /// PnlRoomPopupController controller for PnlRoomPopup
 /// </summary>
 public class PnlRoomPopupController {
-    private RoomPopupShowChecker _roomPopupShowChecker;
+    private PopupShowChecker _roomPopupShowChecker;
     private StreamerCountUpdater _streamerCountUpdater;
 
     private RoomJsonData _receivedRoomJsonData;
@@ -25,11 +25,11 @@ public class PnlRoomPopupController {
     private bool _isWaitIfNeedHideStarted;
     private bool _isCheckStateStarted;
 
-    public PnlRoomPopupController(RoomPopupShowChecker roomPopupShowChecker, StreamerCountUpdater streamerCountUpdater) {
+    public PnlRoomPopupController(PopupShowChecker roomPopupShowChecker, StreamerCountUpdater streamerCountUpdater) {
         Construct(roomPopupShowChecker, streamerCountUpdater);
     }
 
-    private void Construct(RoomPopupShowChecker roomPopupShowChecker, StreamerCountUpdater streamerCountUpdater) {
+    private void Construct(PopupShowChecker roomPopupShowChecker, StreamerCountUpdater streamerCountUpdater) {
         _roomPopupShowChecker = roomPopupShowChecker;
         _streamerCountUpdater = streamerCountUpdater;
 
