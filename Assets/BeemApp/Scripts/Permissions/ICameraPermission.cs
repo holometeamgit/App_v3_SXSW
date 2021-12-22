80d3799911049d24045b356e37e57b7cbb2a51a0
dsharabarov <dsharabarov@axmor.com>
2021-12-22 16:27:07 +0700

    Added summary

--- Assets/BeemApp/Scripts/Permissions/ICameraPermission.cs
+++ Assets/BeemApp/Scripts/Permissions/ICameraPermission.cs
@@ -6,7 +6,17 @@ namespace Beem.Permissions {
     /// Permission for Camera
     /// </summary>
     public interface ICameraPermission {
+
+        /// <summary>
+        /// Has Camera Access
+        /// </summary>
         bool HasCameraAccess { get; }
+
+        /// <summary>
+        /// Request Camera Access
+        /// </summary>
+        /// <param name="onSuccessed"></param>
+        /// <param name="onFailed"></param>
         void RequestCameraAccess(Action onSuccessed, Action onFailed);
     }
 }
