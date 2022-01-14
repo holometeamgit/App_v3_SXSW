dba80fabac196cfe8eaff0d8ddfc2d0f97075fae
Zuheb <zed_jay_1@hotmail.com>
2022-01-14 16:32:48 +0000

    -Updated naming of events to get around schema discarded issue

--- Assets/HoloMeApp/Scripts/Analytics/AnalyticKeys.cs
+++ Assets/HoloMeApp/Scripts/Analytics/AnalyticKeys.cs
@@ -1,62 +1,68 @@
 ﻿public class AnalyticKeys {
-    public const string KeyRegistrationComplete = "registrationComplete";
-    public const string KeyRegistrationDropOff = "registrationDroppedOff";
-    public const string KeyUserSignup = "userSignup";
-    public const string KeyUserLogin = "userLogin";
+    public const string KeyRegistrationComplete = "Registration_Complete";
+    public const string KeyRegistrationDropOff = "Registration_Dropped_Off";
+    public const string KeyUserSignup = "User_Signup";
+    public const string KeyUserLogin = "User_Login";
 
-    public const string KeySignUpTapped = "signUpTapped";
-    public const string KeyProfileCreated = "profileCreated";
-    public const string KeyEmailOptIn = "emailOptIn";
-    //public const string KeyNotificationOptIn = "notificationOptIn"; //TODO future opt in notifications panel
-    //public const string KeyNotificationMaybeLater = "notificationMaybeLater"; //TODO future opt in notifications panel
+    public const string KeySignUpTapped = "Sign_Up_Tapped";
+    public const string KeyProfileCreated = "Profile_Created";
+    public const string KeyEmailOptIn = "Email_Opt_In";
+    //public const string KeyNotificationOptIn = "Notification_Opt_In"; //TODO future opt in notifications panel
+    //public const string KeyNotificationMaybeLater = "Notification_Maybe_Later"; //TODO future opt in notifications panel
 
-    public const string KeyShareHologramMediaPressed = "shareHologramRecordingPressed";
-    public const string KeySnapshotShared = "hologramSnapshotShared";
-    public const string KeyVideoShared = "hologramVideoShared";
+    public const string KeyShareHologramMediaPressed = "Share_Hologram_Recording_Pressed";
+    public const string KeySnapshotShared = "Hologram_Snapshot_Shared";
+    public const string KeyVideoShared = "Hologram_Video_Shared";
 
-    public const string KeyHologramViewPercentage = "hologramViewPercentage";
-    public const string KeyHologramLiveViewTime = "hologramLiveViewTime";
-    public const string KeyViewerCountUpdate = "viewerCountUpdate";
-    public const string KeyMaxViewerCount = "maxViewerCount";
+    public const string KeyHologramViewPercentage = "Hologram_View_Percentage";
+    public const string KeyHologramLiveViewTime = "Hologram_Live_View_Time";
+    public const string KeyViewerCountUpdate = "Viewer_Count_Update";
+    public const string KeyMaxViewerCount = "Max_Viewer_Count";
 
-    public const string KeyGoLive = "goLive"; //When a broadcaster clicks 'go live' from the main menu
-    public const string KeyLiveStarted = "liveStarted"; //When a broadcaster clicks 'go live' from the main menu and starts to record a video
+    public const string KeyBeemMeSelected = "Beem_Me_Pressed";
+    public const string KeyBeemMeUploadStarted = "Upload_Beem_Me";
+    public const string KeyBeemMeConversionComplete = "Conversion_Complete";
+    public const string KeyBeemMePlaced = "Place_Beem_Me";
+    public const string KeyBeemMeShared = "Share_Beem_Me";
 
-    public const string KeyStartPerformance = "startPerformance"; //The user clicks 'watch now', before placing the hologram in their environment
-    public const string KeyPerformanceLoaded = "performanceLoaded";
-    public const string KeyPerformanceNotEnded = "performanceNotEnded";
-    public const string KeyPerformanceEnded = "performanceEnded"; //Watched full hologram
+    public const string KeyGoLive = "Go_Live"; //When a broadcaster clicks 'go live' from the main menu
+    public const string KeyLiveStarted = "Live_Started"; //When a broadcaster clicks 'go live' from the main menu and starts to record a video
 
-    public const string KeyPurchasePressed = "purchasePressed";
-    public const string KeyPurchaseSuccessful = "purchaseSuccessful";
-    public const string KeyPurchaseFailed = "purchaseFailed";
-    public const string KeyPurchaseCancelled = "purchaseCancelled";
-    public const string KeyShareEventPressed = "shareEventPressed";
+    public const string KeyStartPerformance = "Start_Performance"; //The user clicks 'watch now', before placing the hologram in their environment
+    public const string KeyPerformanceLoaded = "Performance_Loaded";
+    public const string KeyPerformanceNotEnded = "Performance_Not_Ended"; //Whole performance wasn't watched
+    public const string KeyPerformanceEnded = "Performance_Ended"; //Watched full hologram
 
-    public const string KeySessionLength = "sessionLength";
-    public const string KeyViewLengthOfStream = "lengthOfStream";
+    public const string KeyPurchasePressed = "Purchase_Pressed";
+    public const string KeyPurchaseSuccessful = "Purchase_Successful";
+    public const string KeyPurchaseFailed = "Purchase_Failed";
+    public const string KeyPurchaseCancelled = "Purchase_Cancelled";
+    public const string KeyShareEventPressed = "Share_Event_Pressed";
 
-    public const string KeyHomeScreen = "homeScreen";
-    public const string KeySettingsPanel = "settings";
+    public const string KeySessionLength = "Session_Length";
+    public const string KeyViewLengthOfStream = "Length_Of_Stream_Viewed";
+
+    public const string KeyHomeScreen = "Home_Screen";
+    public const string KeySettingsPanel = "Settings";
 }
 
 public class AnalyticParameters {
-    public const string ParamUserID = "userID";
-    public const string ParamProductID = "productId";
-    public const string ParamBroadcasterUserID = "broadcasterUserId";
-    public const string ParamPerformanceID = "performanceID";
-    public const string ParamUserEmail = "userEmail";
-    public const string ParamUserType = "userType";
-    public const string ParamIsRoom = "isRoom";
-    public const string ParamViewer = "viewer";
-    public const string ParamBroadcaster = "broadcaster";
-    public const string ParamVideoName = "videoName";
-    public const string ParamEventName = "eventName";
-    public const string ParamProductPrice = "amount";
-    public const string ParamTime = "time";
-    public const string ParamDate = "date";
-    public const string ParamSignUpMethod = "signUpMethod";
-    public const string ParamViewerCount = "viewerCount";
-    public const string ParamChannelName = "channelName";
+    public const string ParamUserID = "User_ID";
+    public const string ParamProductID = "Product_ID";
+    public const string ParamBroadcasterUserID = "Broadcaster_User_ID";
+    public const string ParamPerformanceID = "Performance_ID";
+    public const string ParamUserEmail = "User_Email";
+    public const string ParamUserType = "User_Type";
+    public const string ParamIsRoom = "Is_Room";
+    public const string ParamViewer = "Viewer";
+    public const string ParamBroadcaster = "Broadcaster";
+    public const string ParamVideoName = "Video_Name";
+    public const string ParamEventName = "Event_Name";
+    public const string ParamProductPrice = "Amount";
+    public const string ParamTime = "Time";
+    public const string ParamDate = "Date";
+    public const string ParamSignUpMethod = "Sign_Up_Method";
+    public const string ParamViewerCount = "Viewer_Count";
+    public const string ParamChannelName = "Channel_Name";
     //public const string ParamNotificationsOptIn = "notificationOptIn"; //TODO future opt in notifications panel
 }
