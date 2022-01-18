ad952a351eee4a6d503c4dca76db5df05cd5d39f
dsharabarov <dsharabarov@axmor.com>
2022-01-18 09:45:06 +0700

    Added unsubcribe

--- Assets/HoloMeApp/Scripts/Analytics/AnalyticsBeemMeCallbacks.cs
+++ Assets/HoloMeApp/Scripts/Analytics/AnalyticsBeemMeCallbacks.cs
@@ -6,9 +6,29 @@ using UnityEngine;
 /// </summary>
 public class AnalyticsBeemMeCallbacks : MonoBehaviour {
 
-    void Start() {
-        CallBacks.OnStartRecord += () => AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeRecordStarted);
-        CallBacks.OnARMsgUpdloaded += () => AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeUploadComplete);
-        CallBacks.OnARMsgByIdReceived += x => { if (x.processing_status == ARMsgJSON.Data.COMPETED_STATUS) { AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeConversionComplete); } };
+    private void Start() {
+        CallBacks.OnStartRecord += OnStartRecordHandler;
+        CallBacks.OnARMsgUpdloaded += OnARMsgUpdloadedHandler;
+        CallBacks.OnARMsgByIdReceived += OnARMsgByIdReceivedHandler;
+    }
+
+    private void OnDestroy() {
+        CallBacks.OnStartRecord -= OnStartRecordHandler;
+        CallBacks.OnARMsgUpdloaded -= OnARMsgUpdloadedHandler;
+        CallBacks.OnARMsgByIdReceived -= OnARMsgByIdReceivedHandler;
+    }
+
+    private void OnStartRecordHandler() {
+        AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeRecordStarted);
+    }
+
+    private void OnARMsgUpdloadedHandler() {
+        AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeUploadComplete);
+    }
+
+    private void OnARMsgByIdReceivedHandler(ARMsgJSON.Data data) {
+        if (data.processing_status == ARMsgJSON.Data.COMPETED_STATUS) {
+            AnalyticsController.Instance.SendCustomEvent(AnalyticKeys.KeyBeemMeConversionComplete);
+        };
     }
 }
