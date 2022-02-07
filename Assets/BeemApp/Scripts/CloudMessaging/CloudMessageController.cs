df0ce6ac63819c71611e2074b2b5cd6179d96069
dsharabarov <dsharabarov@axmor.com>
2022-02-07 11:45:42 +0700

    fix Log

--- Assets/BeemApp/Scripts/CloudMessaging/CloudMessageController.cs
+++ Assets/BeemApp/Scripts/CloudMessaging/CloudMessageController.cs
@@ -15,8 +15,6 @@ namespace Beem.Firebase.CloudMessage {
     /// </summary>
     public class CloudMessageController : MonoBehaviour {
 
-        private const string SENDER_ID = "233061171188";
-
         private const string TOPIC = "Test";
 
         private void OnEnable() {
@@ -24,7 +22,6 @@ namespace Beem.Firebase.CloudMessage {
         }
 
         protected void Subscribe() {
-            Debug.LogError("Subscribe");
             FirebaseMessaging.TokenReceived += OnTokenReceived;
             FirebaseMessaging.MessageReceived += OnMessageReceived;
             FirebaseMessaging.SubscribeAsync(TOPIC);
@@ -40,21 +37,14 @@ namespace Beem.Firebase.CloudMessage {
 
         private void OnTokenReceived(object sender, TokenReceivedEventArgs token) {
             GUIUtility.systemCopyBuffer = token.Token;
-            HelperFunctions.DevLogError("Received Registration Token: " + token.Token);
+            HelperFunctions.DevLog("Received Registration Token: " + token.Token);
         }
 
         private void OnMessageReceived(object sender, MessageReceivedEventArgs e) {
-
-            HelperFunctions.DevLogError($"Message ID: {e.Message.MessageId}");
-
             if (e.Message.Data.ContainsKey("dl")) {
+                HelperFunctions.DevLog($"Message Deep Link: {e.Message.MessageId}");
                 DynamicLinksCallBacks.onReceivedDeepLink?.Invoke(e.Message.Data["dl"]);
             }
-
-            foreach (KeyValuePair<string, string> item in e.Message.Data) {
-                HelperFunctions.DevLogError($"Message Data: {item.Key} , {item.Value}");
-            }
-
         }
 
         /*
