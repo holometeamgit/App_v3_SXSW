954fb2320bb7f25d469d878491156da591ed468f
IvanKlimov1 <ivan.klimov@holo.me>
2022-02-16 11:26:28 +0300

    Update copy for both cameras

---
+++ Assets/HoloMeApp/Scripts/ARMsg/ARMsgTextHelp.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Beem.ARMsg;
+
+public class ARMsgTextHelp : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _text;
+
+    [SerializeField]
+    [TextArea]
+    private string _stringForBackCamera =
+        "The recording will start on the next\n" +
+        "screen.Make sure the person you are\n" +
+        "recording is in full frame throughout";
+
+    [SerializeField]
+    [TextArea]
+    private string _stringForFrontCamera = "";
+
+
+    private void OnEnable() {
+        CallBacks.onCameraSwitched += UpdateText;
+        if(CallBacks.onGetCurrentCameraID != null)
+            UpdateText(CallBacks.onGetCurrentCameraID.Invoke());
+    }
+
+    private void UpdateText(int cameraID) {
+        if(cameraID == ARMsgCameraPreview.BACK_CAMERA) {
+            _text.text = _stringForBackCamera;
+        } else {
+            _text.text = _stringForFrontCamera;
+        }
+    }
+
+    private void OnDisable() {
+        CallBacks.onCameraSwitched -= UpdateText;
+    }
+}
