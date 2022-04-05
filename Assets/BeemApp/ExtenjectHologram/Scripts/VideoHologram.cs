8b3e0a381dc88d37f94f6c0b9f785c61295257cb
dsharabarov <dsharabarov@axmor.com>
2022-04-05 14:15:18 +0700

    Test

---
+++ Assets/BeemApp/ExtenjectHologram/Scripts/VideoHologram.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoHologram : IHologram {
+
+    public void Play() {
+        throw new System.NotImplementedException();
+    }
+
+    public void Stop() {
+        throw new System.NotImplementedException();
+    }
+}
