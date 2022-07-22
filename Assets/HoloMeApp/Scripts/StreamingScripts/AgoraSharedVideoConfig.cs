cc3b17ff71e4c7ca7a69aed432ff5fd9bdf327c8
Zuheb <zuheb.javed@holome.co.uk>
2022-07-22 12:40:23 +0100

    -Fixed Natcorder crash on android devices

--- Assets/HoloMeApp/Scripts/StreamingScripts/AgoraSharedVideoConfig.cs
+++ Assets/HoloMeApp/Scripts/StreamingScripts/AgoraSharedVideoConfig.cs
@@ -1,13 +1,12 @@
 ﻿
 using agora_gaming_rtc;
 
-public class AgoraSharedVideoConfig
-{
+public class AgoraSharedVideoConfig {
     public const int FrameRate = (int)FRAME_RATE.FRAME_RATE_FPS_30;
     public const int Bitrate = 2000;
     private const int MAX_HEIGHT = 1280;
 
-    public static void  GetResolution(int screenWidth, int screenHeigh, out int width, out int heigh) {
+    public static void GetResolution(int screenWidth, int screenHeigh, out int width, out int heigh) {
         float ratio = ((float)screenWidth) / screenHeigh;
 
         heigh = MAX_HEIGHT;
@@ -19,5 +18,12 @@ public class AgoraSharedVideoConfig
 
         heigh = maxHeigh;
         width = (int)(maxHeigh * ratio);
+
+        heigh = MakeEven(heigh);
+        width = MakeEven(width);
+    }
+
+    private static int MakeEven(int value) {
+        return value % 2 == 0 ? value : value - 1;
     }
 }
